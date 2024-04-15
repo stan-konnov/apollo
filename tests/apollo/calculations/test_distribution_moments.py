9908c63fe14f7c568038aed49da490f7e23edc7d
Stan Konnov <stanis.konnov@gmail.com>
2024-04-15 22:33:45 +0200

    Testing distribution moments

--- tests/apollo/calculations/test_distribution_moments.py
+++ tests/apollo/calculations/test_distribution_moments.py
@@ -60,3 +60,46 @@ def test__calculate_distribution_moments__for_correct_rolling_window(
     assert dataframe["kurt"].isna().sum() == ignored_rows_count
     assert dataframe["z_score"].isna().sum() == ignored_rows_count
 
+
+@pytest.mark.usefixtures("dataframe")
+@pytest.mark.usefixtures("window_size")
+def test__calculate_distribution_moments__for_correct_moments_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_distribution_moments method for correct moments calculation.
+
+    Resulting dataframe must have correct values for each moment.
+    """
+
+    control_dataframe = dataframe.copy()
+
+    rolling_window = control_dataframe["adj close"].rolling(window=window_size)
+
+    control_dataframe["avg"] = rolling_window.mean()
+
+    control_dataframe["std"] = rolling_window.std()
+
+    control_dataframe["skew"] = rolling_window.skew()
+
+    control_dataframe["kurt"] = rolling_window.kurt()
+
+    control_dataframe["z_score"] = (
+        (control_dataframe["adj close"] - control_dataframe["avg"]) /
+        control_dataframe["std"]
+    )
+
+
+    dm_calculator = DistributionMomentsCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+
+    dm_calculator.calculate_distribution_moments()
+
+    pd.testing.assert_series_equal(dataframe["avg"], control_dataframe["avg"])
+    pd.testing.assert_series_equal(dataframe["std"], control_dataframe["std"])
+    pd.testing.assert_series_equal(dataframe["skew"], control_dataframe["skew"])
+    pd.testing.assert_series_equal(dataframe["kurt"], control_dataframe["kurt"])
+    pd.testing.assert_series_equal(dataframe["z_score"], control_dataframe["z_score"])
