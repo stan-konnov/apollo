02f4bbf2f3f7f1d4fc52ba3abfd65be7381ba6e6
Stan Konnov <stanis.konnov@gmail.com>
2024-04-16 21:28:04 +0200

    Simplifying fixture usage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 pytest_plugins = [
     "tests.fixtures.api_response",
-    "tests.fixtures.data_directory_and_file",
+    "tests.fixtures.directory_and_file",
     "tests.fixtures.window_size_and_dataframe",
 ]
