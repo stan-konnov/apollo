70985745e0aff5da051d63691ad37f6df8387ffe
Stan Konnov <stanis.konnov@gmail.com>
2024-04-16 17:53:11 +0200

    More tests for linear regression calculations

--- tests/apollo/calculations/test_price_channels.py
+++ tests/apollo/calculations/test_price_channels.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import pytest
+from scipy.stats import linregress
 
 from apollo.calculations.price_channels import PriceChannelsCalculator
 
@@ -86,6 +88,7 @@ def test__calculate_price_channels__for_correct_indices(
     and reset back to date after calculation.
 
     Resulting dataframe must have "date" as index.
+    Resulting dataframe must have "date" as index dtype.
     """
 
     pc_calculator = PriceChannelsCalculator(
@@ -98,3 +101,100 @@ def test__calculate_price_channels__for_correct_indices(
 
     assert dataframe.index.name == "date"
     assert dataframe.index.dtype == "datetime64[ns]"
+
+
+@pytest.mark.usefixtures("dataframe")
+@pytest.mark.usefixtures("window_size")
+def test__calculate_price_channels__for_correct_linear_regression_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_price_channels method for correct linear regression calculation.
+
+    Resulting dataframe must have correct
+    values for slopes, bounds, and line of best fit.
+    """
+
+    t_slope = np.full((1, window_size - 1), np.nan).flatten().tolist()
+    l_bound = np.full((1, window_size - 1), np.nan).flatten().tolist()
+    u_bound = np.full((1, window_size - 1), np.nan).flatten().tolist()
+    bf_line = np.full((1, window_size - 1), np.nan).flatten().tolist()
+
+    control_dataframe = dataframe.copy()
+    control_dataframe.reset_index(inplace=True)
+
+    control_dataframe["adj close"].rolling(window_size).apply(
+        mimic_calc_lin_reg,
+        args=(
+            t_slope,
+            l_bound,
+            u_bound,
+            bf_line,
+            CHANNEL_SD_SPREAD,
+        ),
+    )
+
+    control_dataframe.set_index("date", inplace=True)
+
+    control_dataframe["slope"] = t_slope
+    control_dataframe["prev_slope"] = control_dataframe["slope"].shift(1)
+
+    control_dataframe["l_bound"] = l_bound
+    control_dataframe["u_bound"] = u_bound
+
+    control_dataframe["lbf"] = bf_line
+
+    pc_calculator = PriceChannelsCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+        channel_sd_spread=CHANNEL_SD_SPREAD,
+    )
+
+    pc_calculator.calculate_price_channels()
+
+    pd.testing.assert_series_equal(dataframe["slope"], control_dataframe["slope"])
+    pd.testing.assert_series_equal(
+        dataframe["prev_slope"],
+        control_dataframe["prev_slope"],
+    )
+
+    pd.testing.assert_series_equal(dataframe["l_bound"], control_dataframe["l_bound"])
+    pd.testing.assert_series_equal(dataframe["u_bound"], control_dataframe["u_bound"])
+
+    pd.testing.assert_series_equal(dataframe["lbf"], control_dataframe["lbf"])
+
+
+def mimic_calc_lin_reg(
+        series: pd.Series,
+        t_slope: list[float],
+        l_bound: list[float],
+        u_bound: list[float],
+        bf_line: list[float],
+        channel_sd_spread: float,
+    ) -> float:
+    """
+    Mimicry of linear regression calculation for testing purposes.
+
+    Please see Price Channels calculator for
+    detailed explanation of linear regression calculation.
+    """
+
+    x = series.index
+    y = series.to_numpy()
+
+    slope, intercept, _, _, _ = linregress(x, y)
+    t_slope.append(slope)
+
+    lbf = (slope * x + intercept)
+    bf_line.append(lbf[-1])
+
+    std = y.std()
+
+    lower_bound = lbf - std * channel_sd_spread
+    upper_bound = lbf + std * channel_sd_spread
+
+    l_bound.append(lower_bound[-1])
+    u_bound.append(upper_bound[-1])
+
+    return 0.0
