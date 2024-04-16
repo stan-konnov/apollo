4fb29acea01a0417c973c4481583e436fa015bbe
Stan Konnov <stanis.konnov@gmail.com>
2024-04-16 23:12:52 +0200

    Continue with configuration testing

--- tests/fixtures/window_size_and_dataframe.py
+++ tests/fixtures/window_size_and_dataframe.py
@@ -12,7 +12,7 @@ def get_price_dataframe() -> pd.DataFrame:
     """Fixture to get test dataframe from file system."""
 
     test_dataframe = pd.read_csv(
-        Path(f"{Path(curdir).resolve()}/tests/test_data/test.csv"),
+        Path(f"{Path(curdir).resolve()}/tests/test_data/test_prices.csv"),
         index_col=0,
     )
     test_dataframe.index = pd.to_datetime(test_dataframe.index)
