d618e19432e3592d450b356a458f9717d71ba964
Stan Konnov <stanis.konnov@gmail.com>
2024-04-16 21:37:02 +0200

    Expanding fixtures

--- tests/fixtures/directory_and_file.py
+++ tests/fixtures/directory_and_file.py
@@ -9,14 +9,16 @@ import pytest
 from apollo.settings import (
     END_DATE,
     START_DATE,
+    STRATEGY,
     TICKER,
     ValidYahooApiFrequencies,
 )
 
-DATA_DIR = Path(f"{Path(curdir).resolve()}/tests/temp")
+TEMP_DIR = Path(f"{Path(curdir).resolve()}/tests/temp")
+PARM_FIL = Path(f"{TEMP_DIR}/{STRATEGY}.json")
 DATA_FIL = Path(
     str(
-        f"{DATA_DIR}/{TICKER}-"
+        f"{TEMP_DIR}/{TICKER}-"
         f"{ValidYahooApiFrequencies.ONE_DAY.value}-"
         f"{START_DATE}-{END_DATE}.csv",
     ),
@@ -24,9 +26,9 @@ DATA_FIL = Path(
 
 @pytest.fixture(name="data_dir", scope="session")
 def _get_data_dir() -> Generator[None, None, None]:
-    """Fixture to get data directory."""
+    """Fixture to get data directory for testing Yahoo API Connector."""
 
-    with patch("apollo.api.yahoo_api_connector.DATA_DIR", DATA_DIR):
+    with patch("apollo.api.yahoo_api_connector.DATA_DIR", TEMP_DIR):
         yield
 
 
@@ -37,9 +39,24 @@ def get_data_file() -> Path:
     return DATA_FIL
 
 
+@pytest.fixture(name="parameters_dir", scope="session")
+def _get_parameters_dir() -> Generator[None, None, None]:
+    """Fixture to get parameters directory for testing Configuration."""
+
+    with patch("apollo.utils.configuration.PARM_DIR", TEMP_DIR):
+        yield
+
+
+@pytest.fixture(name="parameters_file", scope="session")
+def get_parameters_file() -> Path:
+    """Fixture to get parameters file."""
+
+    return PARM_FIL
+
+
 @pytest.fixture(scope="session", autouse=True)
 def _clean_data() -> Generator[None, None, None]:
     """Clean temp test data directory after tests."""
 
     yield
-    rmtree(DATA_DIR)
+    rmtree(TEMP_DIR)
