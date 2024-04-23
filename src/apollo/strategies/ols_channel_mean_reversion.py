1c894db2e27e2332089914ca6676d036c2f4e6b6
Stan Konnov <stanis.konnov@gmail.com>
2024-04-23 18:50:58 +0200

    Reworking old logic from legacy

---
+++ src/apollo/strategies/ols_channel_mean_reversion.py
@@ -0,0 +1,63 @@
+from pandas import DataFrame
+
+from apollo.calculations.price_channels import PriceChannelsCalculator
+from apollo.settings import LONG_SIGNAL, SHORT_SIGNAL
+from apollo.strategies.base import BaseStrategy
+
+
+class OrdinaryLeastSquaresChannelMeanReversion(BaseStrategy):
+    """OrdinaryLeastSquaresChannelsMeanReversion."""
+
+    def __init__(
+        self,
+        dataframe: DataFrame,
+        window_size: int,
+        channel_sd_spread: float,
+    ) -> None:
+        """
+        Construct.
+
+        :param dataframe: Dataframe with price data.
+        :param window_size: Size of the window for the strategy.
+        :param channel_sd_spread: Standard deviation spread for channel bounds.
+        """
+
+        self._validate_parameters(
+            [
+                ("channel_sd_spread", channel_sd_spread, float),
+            ],
+        )
+
+        super().__init__(dataframe, window_size)
+
+        self.pc_calculator = PriceChannelsCalculator(
+            dataframe,
+            window_size,
+            channel_sd_spread,
+        )
+
+
+    def model_trading_signals(self) -> None:
+        """Model entry and exit signals."""
+
+        self.__calculate_indicators()
+        self.__mark_trading_signals()
+        self.dataframe.dropna(inplace=True)
+
+
+    def __calculate_indicators(self) -> None:
+        """Calculate indicators necessary for the strategy."""
+
+        self.pc_calculator.calculate_price_channels()
+
+
+    def __mark_trading_signals(self) -> None:
+        """Mark long and short signals based on the strategy."""
+
+        self.dataframe.loc[
+            self.dataframe["adj close"] <= self.dataframe["l_bound"], "signal",
+        ] = LONG_SIGNAL
+
+        self.dataframe.loc[
+            self.dataframe["adj close"] >= self.dataframe["u_bound"], "signal",
+        ] = SHORT_SIGNAL
