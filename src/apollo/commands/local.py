ca5667188e79b8bb68dc7861d7d0ad02d8318d33
Stan Konnov <stanis.konnov@gmail.com>
2024-04-23 22:10:25 +0200

    Slicing in more complex stuff from Kaufman

--- src/apollo/commands/local.py
+++ src/apollo/commands/local.py
@@ -1,13 +1,13 @@
 import logging
 
-# from apollo.backtesting.parameter_optimizer import ParameterOptimizer
+from apollo.backtesting.parameter_optimizer import ParameterOptimizer
 
-from apollo.api.yahoo_api_connector import YahooApiConnector
-from apollo.backtesting.backtesting_runner import BacktestingRunner
-from apollo.settings import END_DATE, START_DATE, TICKER
-from apollo.strategies.ols_channel_mean_reversion import (
-    OrdinaryLeastSquaresChannelMeanReversion,
-)
+# from apollo.api.yahoo_api_connector import YahooApiConnector
+# from apollo.backtesting.backtesting_runner import BacktestingRunner
+# from apollo.settings import END_DATE, START_DATE, TICKER
+# from apollo.strategies.ols_channel_mean_reversion import (
+#     OrdinaryLeastSquaresChannelMeanReversion,
+# )
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,36 +20,36 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     """Run local scripts to quickly iterate on code."""
 
-    # parameter_optimizer = ParameterOptimizer()
-    # parameter_optimizer.process()
+    parameter_optimizer = ParameterOptimizer()
+    parameter_optimizer.process()
 
-    api_connector = YahooApiConnector(
-        ticker=str(TICKER),
-        start_date=str(START_DATE),
-        end_date=str(END_DATE),
-    )
+    # api_connector = YahooApiConnector(
+    #     ticker=str(TICKER),
+    #     start_date=str(START_DATE),
+    #     end_date=str(END_DATE),
+    # )
 
-    dataframe = api_connector.request_or_read_prices()
+    # dataframe = api_connector.request_or_read_prices()
 
-    strategy = OrdinaryLeastSquaresChannelMeanReversion(
-        dataframe=dataframe,
-        window_size=20,
-        channel_sd_spread=0.5,
-    )
+    # strategy = OrdinaryLeastSquaresChannelMeanReversion(
+    #     dataframe=dataframe,
+    #     window_size=20,
+    #     channel_sd_spread=0.5,
+    # )
 
-    strategy.model_trading_signals()
+    # strategy.model_trading_signals()
 
-    backtesting_runner = BacktestingRunner(
-        dataframe=dataframe,
-        strategy_name="ols_channel_mean_reversion",
-        lot_size_cash=1000,
-        stop_loss_level=0.005,
-        take_profit_level=0.05,
-    )
+    # backtesting_runner = BacktestingRunner(
+    #     dataframe=dataframe,
+    #     strategy_name="ols_channel_mean_reversion",
+    #     lot_size_cash=1000,
+    #     stop_loss_level=0.005,
+    #     take_profit_level=0.05,
+    # )
 
-    stats = backtesting_runner.run()
+    # stats = backtesting_runner.run()
 
-    print(stats)
+    # print(stats)
 
 
 if __name__ == "__main__":
