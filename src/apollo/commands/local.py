7284db1d242a3a1f9b6ff52493de6ac314199320
Stan Konnov <stanis.konnov@gmail.com>
2024-04-30 16:16:57 +0200

    More backtesting

--- src/apollo/commands/local.py
+++ src/apollo/commands/local.py
@@ -1,11 +1,11 @@
 import logging
 
-# from apollo.backtesting.parameter_optimizer import ParameterOptimizer
+from apollo.backtesting.parameter_optimizer import ParameterOptimizer
 
-from apollo.api.yahoo_api_connector import YahooApiConnector
-from apollo.backtesting.backtesting_runner import BacktestingRunner
-from apollo.settings import END_DATE, START_DATE, TICKER
-from apollo.strategies.swing_events_mean_reversion import SwingEventsMeanReversion
+# from apollo.api.yahoo_api_connector import YahooApiConnector
+# from apollo.backtesting.backtesting_runner import BacktestingRunner
+# from apollo.settings import END_DATE, START_DATE, TICKER
+# from apollo.strategies.swing_events_mean_reversion import SwingEventsMeanReversion
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,37 +18,37 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     """Run local scripts to quickly iterate on code."""
 
-    # parameter_optimizer = ParameterOptimizer()
-    # parameter_optimizer.process()
+    parameter_optimizer = ParameterOptimizer()
+    parameter_optimizer.process()
 
-    yahoo_api_connector = YahooApiConnector(
-        ticker=str(TICKER),
-        start_date=str(START_DATE),
-        end_date=str(END_DATE),
-    )
+    # yahoo_api_connector = YahooApiConnector(
+    #     ticker=str(TICKER),
+    #     start_date=str(START_DATE),
+    #     end_date=str(END_DATE),
+    # )
 
-    dataframe = yahoo_api_connector.request_or_read_prices()
+    # dataframe = yahoo_api_connector.request_or_read_prices()
 
-    strategy = SwingEventsMeanReversion(
-        dataframe=dataframe,
-        window_size=5,
-        swing_filter=0.01,
-    )
+    # strategy = SwingEventsMeanReversion(
+    #     dataframe=dataframe,
+    #     window_size=5,
+    #     swing_filter=0.01,
+    # )
 
-    strategy.model_trading_signals()
+    # strategy.model_trading_signals()
 
-    backtesting_runner = BacktestingRunner(
-        dataframe=dataframe,
-        strategy_name="SwingEventsMeanReversion",
-        lot_size_cash=1000,
-        sl_volatility_multiplier=0.1,
-        tp_volatility_multiplier=0.3,
-        write_result_plot=True,
-    )
+    # backtesting_runner = BacktestingRunner(
+    #     dataframe=dataframe,
+    #     strategy_name="SwingEventsMeanReversion",
+    #     lot_size_cash=1000,
+    #     sl_volatility_multiplier=0.1,
+    #     tp_volatility_multiplier=0.3,
+    #     write_result_plot=True,
+    # )
 
-    stats = backtesting_runner.run()
+    # stats = backtesting_runner.run()
 
-    print(stats)
+    # print(stats)
 
 
 if __name__ == "__main__":
