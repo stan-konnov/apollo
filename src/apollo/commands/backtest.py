c3b6b0f068f7f6d11bdd1eb2e5e4cfb49464a5c1
Stan Konnov <stanis.konnov@gmail.com>
2024-05-02 21:22:20 +0200

    Just playing around

--- src/apollo/commands/backtest.py
+++ src/apollo/commands/backtest.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 from apollo.api.yahoo_api_connector import YahooApiConnector
 from apollo.backtesting.backtesting_runner import BacktestingRunner
 from apollo.settings import END_DATE, START_DATE, TICKER
-from apollo.strategies.swing_events_mean_reversion import SwingEventsMeanReversion
+from apollo.strategies.linear_regression_forecast import LinearRegressionForecast
 
 if TYPE_CHECKING:
     import pandas as pd
@@ -28,17 +28,17 @@ def main() -> None:
 
     dataframe = yahoo_api_connector.request_or_read_prices()
 
-    strategy = SwingEventsMeanReversion(
+    strategy = LinearRegressionForecast(
         dataframe=dataframe,
         window_size=5,
-        swing_filter=0.01,
+        split_ratio=0.8,
     )
 
     strategy.model_trading_signals()
 
     backtesting_runner = BacktestingRunner(
         dataframe=dataframe,
-        strategy_name="SwingEventsMeanReversion",
+        strategy_name="LinearRegressionForecast",
         lot_size_cash=1000,
         sl_volatility_multiplier=0.1,
         tp_volatility_multiplier=0.3,
@@ -49,8 +49,8 @@ def main() -> None:
 
     logger.info(stats)
 
-    trades: pd.DataFrame = stats["_trades"]
-    trades["ReturnPct"] = trades["ReturnPct"] * 100
+    # trades: pd.DataFrame = stats["_trades"]
+    # trades["ReturnPct"] = trades["ReturnPct"] * 100
 
     # trades.to_csv("_temp_trades.csv")  # noqa: ERA001
 
---
+++ src/apollo/strategies/linear_regression_forecast.py
@@ -0,0 +1,55 @@
+from pandas import DataFrame
+
+from apollo.calculations.models.linear_regression import LinearRegressionModelCalculator
+from apollo.settings import LONG_SIGNAL, SHORT_SIGNAL
+from apollo.strategies.base_strategy import BaseStrategy
+
+
+class LinearRegressionForecast(BaseStrategy):
+    """Linear Regression Forecast."""
+
+    def __init__(
+        self,
+        dataframe: DataFrame,
+        window_size: int,
+        split_ratio: float,
+    ) -> None:
+        """
+        Construct.
+
+        :param dataframe: Dataframe with price data.
+        :param window_size: Size of the window for the strategy.
+        :param split_ratio: Ratio to split data into train and test.
+        """
+
+        self._validate_parameters(
+            [
+                ("split_ratio", split_ratio, float),
+            ],
+        )
+
+        super().__init__(dataframe, window_size)
+
+        self.lr_calculator = LinearRegressionModelCalculator(
+            dataframe=dataframe,
+            window_size=window_size,
+            split_ratio=split_ratio,
+        )
+
+    def model_trading_signals(self) -> None:
+        """Model entry and exit signals."""
+
+        self.__calculate_indicators()
+        self.__mark_trading_signals()
+        self.dataframe.dropna(inplace=True)
+
+    def __calculate_indicators(self) -> None:
+        """Calculate indicators necessary for the strategy."""
+
+        self.lr_calculator.forecast_periods()
+
+    def __mark_trading_signals(self) -> None:
+        """Mark long and short signals based on the strategy."""
+
+        self.dataframe.loc[self.dataframe["forecast"] > 0, "signal"] = LONG_SIGNAL
+        self.dataframe.loc[self.dataframe["forecast"] < 0, "signal"] = SHORT_SIGNAL
