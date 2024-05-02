2be8fa51e858c15278d341683db7903f8f77d7dc
Stan Konnov <stanis.konnov@gmail.com>
2024-05-02 17:30:43 +0200

    Dependencies, training conditions

--- src/apollo/calculations/models/linear_regression.py
+++ src/apollo/calculations/models/linear_regression.py
@@ -1,5 +1,10 @@
-import pandas as pd
+from typing import ClassVar
 
+import pandas as pd
+from sklearn.base import BaseEstimator
+
+# from sklearn.linear_model import Lasso, LinearRegression, Ridge
+# from sklearn.model_selection import train_test_split
 from apollo.calculations.base_calculator import BaseCalculator
 
 
@@ -21,6 +26,10 @@ class LinearRegressionModelCalculator(BaseCalculator):
     * Ridge Regression
     """
 
+    # Stores selected model to apply
+    # after running goodness of fit tests
+    model_to_apply: ClassVar[BaseEstimator]
+
     def __init__(self, dataframe: pd.DataFrame, window_size: int) -> None:
         """
         Construct Linear Regression Model Calculator.
@@ -30,3 +39,34 @@ class LinearRegressionModelCalculator(BaseCalculator):
         """
 
         super().__init__(dataframe, window_size)
+
+    def _create_regression_trading_conditions(
+        self,
+        dataframe: pd.DataFrame,
+    ) -> tuple[pd.DataFrame, pd.Series]:
+        """
+        Create trading conditions to supply to the model.
+
+        We consider our X variable to be the difference between
+        the high and low, and open and close of every observation.
+
+        We consider our Y variable to be the
+        difference between close at T and close at T-1.
+        """
+
+        # Create a copy to avoid modifying original dataframe
+        training_conditions_dataframe = dataframe.copy()
+
+        # Calculate difference between high and low, open and close
+        training_conditions_dataframe["high_low"] = dataframe["high"] - dataframe["low"]
+        training_conditions_dataframe["open_close"] = (
+            dataframe["open"] - dataframe["close"]
+        )
+
+        # Pack into dataframe
+        x = training_conditions_dataframe[["open_close", "high_low"]]
+
+        # Calculate Y variable
+        y = dataframe["close"].shift(1) - dataframe["close"]
+
+        return x, y
---
+++ src/apollo/commands/experiment.py
@@ -0,0 +1,39 @@
+import logging
+
+from apollo.api.yahoo_api_connector import YahooApiConnector
+from apollo.calculations.models.linear_regression import LinearRegressionModelCalculator
+from apollo.settings import END_DATE, START_DATE, TICKER
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s %(asctime)s:\n\n%(message)s\n",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    """Quickly iterate on new concepts."""
+
+    yahoo_api_connector = YahooApiConnector(
+        ticker=str(TICKER),
+        start_date=str(START_DATE),
+        end_date=str(END_DATE),
+    )
+
+    dataframe = yahoo_api_connector.request_or_read_prices()
+
+    linear_regression_calculator = LinearRegressionModelCalculator(
+        dataframe=dataframe,
+        window_size=5,
+    )
+
+    x, y = linear_regression_calculator._create_regression_trading_conditions(
+        dataframe=dataframe,
+    )
+
+    print(x, y)
+
+
+if __name__ == "__main__":
+    main()
