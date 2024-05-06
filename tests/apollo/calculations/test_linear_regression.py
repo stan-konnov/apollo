05873cc3d2986429a39c6549fe68bd424633296a
Stan Konnov <stanis.konnov@gmail.com>
2024-05-06 21:38:04 +0200

    Dishing unit tests

--- tests/apollo/calculations/test_linear_regression.py
+++ tests/apollo/calculations/test_linear_regression.py
@@ -49,3 +49,57 @@ def test__forecast_periods__for_correctly_dropping_first_observation(
     lrm_calculator.forecast_periods()
 
     assert len(dataframe.index) == control_dataframe_length - 1
+
+
+@pytest.mark.usefixtures("dataframe")
+def test__define_explanatory_variables__for_creating_correct_x_variable(
+    dataframe: pd.DataFrame,
+) -> None:
+    """
+    Test define_explanatory_variables method for creating correct X variable.
+
+    Resulting X variable must be a Dataframe containing differences between:
+    OHLC aspects and Close and Adjusted Close.
+    """
+
+    control_dataframe = dataframe.copy()
+
+    ohlc_aspects: dict[str, str] = {
+        "o": "open",
+        "h": "high",
+        "l": "low",
+        "c": "close",
+        "a": "adj close",
+    }
+
+    ohlc_aspects_combinations: list[tuple[str, str]] = [
+        ("o", "h"),
+        ("o", "l"),
+        ("o", "c"),
+        ("h", "l"),
+        ("h", "c"),
+        ("l", "c"),
+        ("c", "a"),
+    ]
+
+    variables_to_apply = []
+
+    for aspect_a, aspect_b in ohlc_aspects_combinations:
+        control_dataframe[f"{aspect_a}_{aspect_b}"] = (
+            control_dataframe[ohlc_aspects[aspect_a]]
+            - control_dataframe[ohlc_aspects[aspect_b]]
+        )
+
+        variables_to_apply.append(f"{aspect_a}_{aspect_b}")
+
+    control_x = control_dataframe[variables_to_apply]
+
+    lrm_calculator = LinearRegressionModelCalculator(
+        dataframe=dataframe,
+        split_ratio=SPLIT_RATIO,
+        smoothing_factor=SMOOTHING_FACTOR,
+    )
+
+    x = lrm_calculator._define_explanatory_variables(dataframe)  # noqa: SLF001
+
+    pd.testing.assert_frame_equal(x, control_x)
