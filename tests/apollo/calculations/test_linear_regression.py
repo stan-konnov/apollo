d1898708e2f40d2884e10a3e1b1bbda58d453cad
Stan Konnov <stanis.konnov@gmail.com>
2024-05-06 18:13:09 +0200

    Some renames and dishing out tests

--- tests/apollo/calculations/test_linear_regression.py
+++ tests/apollo/calculations/test_linear_regression.py
@@ -28,3 +28,28 @@ def test__forecast_periods__for_correct_columns(
     lrm_calculator.forecast_periods()
 
     assert "lrf" in lrm_calculator.dataframe.columns
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__forecast_periods__for_correctly_dropping_first_observation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test forecast_periods method for correctly dropping first observation.
+
+    Resulting dataframe length must be equal to the original dataframe length -1.
+    """
+
+    control_dataframe_length = len(dataframe.copy().index)
+
+    lrm_calculator = LinearRegressionModelCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+        split_ratio=SPLIT_RATIO,
+        smoothing_factor=SMOOTHING_FACTOR,
+    )
+
+    lrm_calculator.forecast_periods()
+
+    assert len(dataframe.index) == control_dataframe_length - 1
