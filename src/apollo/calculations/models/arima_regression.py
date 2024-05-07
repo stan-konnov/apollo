8a0a6f0cbcb343b9b6b4918596998adb247c8449
Stan Konnov <stanis.konnov@gmail.com>
2024-05-07 22:20:46 +0200

    Todos and notes

--- src/apollo/calculations/models/arima_regression.py
+++ src/apollo/calculations/models/arima_regression.py
@@ -6,7 +6,12 @@ This, of course includes ARIMA, as well as linear regression models.
 Therefor, the first step is to make the time
 series stationary by removing trends and seasonality.
 
-This has to happen via transformer and, perhaps, applied to all other calculations.
+This has to happen via data preprocessing
+(e.g. differencing, or seasonal differencing) in parent class.
+This concerns all regression model, not only ARIMA!
+
+E.g., apply differencing to all aspect of OHLCV data.
+And only then feed it into the model.
 
 NOTE: consider SARIMA, which is a seasonal ARIMA model.
 """
