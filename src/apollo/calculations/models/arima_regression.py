86944ce6fc6f242567ba7a5ed13e7028d56947d7
Stan Konnov <stanis.konnov@gmail.com>
2024-05-09 21:26:12 +0200

    Just some wip

--- src/apollo/calculations/models/arima_regression.py
+++ src/apollo/calculations/models/arima_regression.py
@@ -1,26 +1,50 @@
-# The basic ARIMA (AutoRegressive Integrated Moving Average) model does not incorporate
-# exogenous variables. It's a univariate time series model that predicts future values
-# based solely on the past values of the same time series.
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
 
-# However, there are extensions of ARIMA that allow for the inclusion of exogenous variables.
-# One such extension is the SARIMAX (Seasonal AutoRegressive Integrated Moving Average with eXogenous regressors)
-# model. SARIMAX extends ARIMA by incorporating exogenous variables that may help improve
-# the forecast accuracy by capturing additional information not present in the time series itself.
+from apollo.calculations.base_calculator import BaseCalculator
+from apollo.utils.time_series_transformer import TimeSeriesTransformer
 
-# Here's how you can use SARIMAX in Python with exogenous variables:
+"""Look into SARIMAX."""
 
-"""
-from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-# Assuming you have a time series dataframe 'df' with columns 'Y' and 'X'
-# 'Y' represents the endogenous time series, and 'X' represents the exogenous variable
+class ARIMARegressionModelCalculator(BaseCalculator):
+    """
+    ARIMA Regression Model Calculator.
 
-# Define the SARIMAX model
-model = SARIMAX(df['Y'], exog=df['X'], order=(p, d, q), seasonal_order=(P, D, Q, m))
+    (Auto Regressive Integrated Moving Average).
 
-# Fit the model
-results = model.fit()
+    ARIMA is a regression model that predicts future values
+    based solely on the past values of the same time series.
 
-# Generate forecasts
-forecast = results.forecast(steps=n_steps, exog=exog_data)
-"""
+    Therefore, due to the lack of explanatory variables,
+    ARIMA requires the time series to be stationary.
+
+    (Please refer to Time Series Transformer for more information).
+
+    ARIMA operates on three parameters:
+
+    * q: The size of the moving average window.
+    * p: The number of lag observations included in the model.
+    * d: The number of times that the raw observations are differenced.
+
+    Donadio and Ghosh, Algorithmic Trading, 2019, 1st ed.
+    """
+
+    def __init__(self, dataframe: pd.DataFrame, window_size: int) -> None:
+        """Construct ARIMA Regression Model Calculator.."""
+        super().__init__(dataframe, window_size)
+
+    def forecast_periods(self) -> None:
+        """
+        Forecast future periods using ARIMA regression model.
+
+        Stationarize the time series, fit the model, and forecast future periods.
+        """
+
+        # Stationarize the time series
+        time_series = TimeSeriesTransformer.bring_to_stationary(self.dataframe["close"])
+
+        # Params to be computed, WIP
+        model = ARIMA(time_series, order=(2, 0, 2))
+
+        self.dataframe["arf"] = model.fit()
