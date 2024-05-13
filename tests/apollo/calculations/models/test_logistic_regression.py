f2703bd46f011d148bd1d6858880f1a234206207
Stan Konnov <stanis.konnov@gmail.com>
2024-05-13 12:32:03 +0200

    Patching tests

--- tests/apollo/calculations/models/test_logistic_regression.py
+++ tests/apollo/calculations/models/test_logistic_regression.py
@@ -29,80 +29,57 @@ def test__forecast_periods__for_correct_columns(
     assert "lrf" in lrm_calculator.dataframe.columns
 
 
-# @pytest.mark.usefixtures("dataframe")
-# def test__forecast_periods__for_correctly_dropping_first_observation(
-#     dataframe: pd.DataFrame,
-# ) -> None:
-#     """
-#     Test forecast_periods method for correctly dropping first observation.
+@pytest.mark.usefixtures("dataframe")
+def test__define_explanatory_variables__for_creating_correct_x_variable(
+    dataframe: pd.DataFrame,
+) -> None:
+    """
+    Test define_explanatory_variables method for creating correct X variable.
 
-#     Resulting dataframe length must be equal to the original dataframe length - 1.
-#     """
+    Resulting X variable must be a Dataframe containing differences between:
+    OHLC aspects and Close and Adjusted Close.
+    """
 
-#     control_dataframe_length = len(dataframe.copy().index)
+    control_dataframe = dataframe.copy()
 
-#     lrm_calculator = LogisticRegressionModelCalculator(
-#         dataframe=dataframe,
-#         train_size=TRAIN_SIZE,
-#     )
+    ohlc_aspects: dict[str, str] = {
+        "o": "open",
+        "h": "high",
+        "l": "low",
+        "c": "close",
+        "a": "adj close",
+    }
 
-#     lrm_calculator.forecast_periods()
+    ohlc_aspects_combinations: list[tuple[str, str]] = [
+        ("o", "h"),
+        ("o", "l"),
+        ("o", "c"),
+        ("h", "l"),
+        ("h", "c"),
+        ("l", "c"),
+        ("c", "a"),
+    ]
 
-#     assert len(dataframe.index) == control_dataframe_length - 1
+    variables_to_apply = []
 
+    for aspect_a, aspect_b in ohlc_aspects_combinations:
+        control_dataframe[f"{aspect_a}_{aspect_b}"] = (
+            control_dataframe[ohlc_aspects[aspect_a]]
+            - control_dataframe[ohlc_aspects[aspect_b]]
+        )
 
-# @pytest.mark.usefixtures("dataframe")
-# def test__define_explanatory_variables__for_creating_correct_x_variable(
-#     dataframe: pd.DataFrame,
-# ) -> None:
-#     """
-#     Test define_explanatory_variables method for creating correct X variable.
+        variables_to_apply.append(f"{aspect_a}_{aspect_b}")
 
-#     Resulting X variable must be a Dataframe containing differences between:
-#     OHLC aspects and Close and Adjusted Close.
-#     """
+    control_x = control_dataframe[variables_to_apply]
 
-#     control_dataframe = dataframe.copy()
+    lrm_calculator = LogisticRegressionModelCalculator(
+        dataframe=dataframe,
+        train_size=TRAIN_SIZE,
+    )
 
-#     ohlc_aspects: dict[str, str] = {
-#         "o": "open",
-#         "h": "high",
-#         "l": "low",
-#         "c": "close",
-#         "a": "adj close",
-#     }
+    x = lrm_calculator._define_explanatory_variables(dataframe)  # noqa: SLF001
 
-#     ohlc_aspects_combinations: list[tuple[str, str]] = [
-#         ("o", "h"),
-#         ("o", "l"),
-#         ("o", "c"),
-#         ("h", "l"),
-#         ("h", "c"),
-#         ("l", "c"),
-#         ("c", "a"),
-#     ]
-
-#     variables_to_apply = []
-
-#     for aspect_a, aspect_b in ohlc_aspects_combinations:
-#         control_dataframe[f"{aspect_a}_{aspect_b}"] = (
-#             control_dataframe[ohlc_aspects[aspect_a]]
-#             - control_dataframe[ohlc_aspects[aspect_b]]
-#         )
-
-#         variables_to_apply.append(f"{aspect_a}_{aspect_b}")
-
-#     control_x = control_dataframe[variables_to_apply]
-
-#     lrm_calculator = LogisticRegressionModelCalculator(
-#         dataframe=dataframe,
-#         split_ratio=SPLIT_RATIO,
-#         smoothing_factor=SMOOTHING_FACTOR,
-#     )
-
-#     x = lrm_calculator._define_explanatory_variables(dataframe)  # noqa: SLF001
-
-#     pd.testing.assert_frame_equal(x, control_x)
+    pd.testing.assert_frame_equal(x, control_x)
 
 
 # @pytest.mark.usefixtures("dataframe")
