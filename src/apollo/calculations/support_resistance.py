5aca43920f68ce2690c44b18a6b6f6ac629a69db
Stan Konnov <stanis.konnov@gmail.com>
2024-05-21 13:00:06 +0200

    Just a setup for touch counts calculations

--- src/apollo/calculations/support_resistance.py
+++ src/apollo/calculations/support_resistance.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from apollo.calculations.base_calculator import BaseCalculator
@@ -7,9 +8,8 @@ class SupportResistanceCalculator(BaseCalculator):
     """
     Support and Resistance calculator.
 
-    Calculates rolling support and resistance levels
-    based on threshold of tolerance that defines the
-    levels and number of times they are touched by the price.
+    Calculates rolling support and resistance
+    levels based on threshold of tolerance.
     """
 
     def __init__(
@@ -18,8 +18,6 @@ class SupportResistanceCalculator(BaseCalculator):
         window_size: int,
         sup_tolerance: float,
         res_tolerance: float,
-        sup_touch_count: float,
-        res_touch_count: float,
     ) -> None:
         """
         Construct Support and Resistance calculator.
@@ -28,13 +26,57 @@ class SupportResistanceCalculator(BaseCalculator):
         :param window_size: Window size for rolling support and resistance calculation.
         :param sup_tolerance: Tolerance for support level calculation.
         :param res_tolerance: Tolerance for resistance level calculation.
-        :param sup_touch_count: Number of times support level is touched.
-        :param res_touch_count: Number of times resistance level is touched.
         """
 
         super().__init__(dataframe, window_size)
 
         self.sup_tolerance = sup_tolerance
         self.res_tolerance = res_tolerance
-        self.sup_touch_count = sup_touch_count
-        self.res_touch_count = res_touch_count
+
+        self.sup_touch_count: list[float] = []
+        self.res_touch_count: list[float] = []
+
+    def calculate_support_resistance(self) -> None:
+        """Calculate rolling support and resistance levels."""
+
+        # Fill support and resistance touch counts
+        # arrays with N NaN, where N = window size
+        self.sup_touch_count = (
+            np.full((1, self.window_size - 1), np.nan).flatten().tolist()
+        )
+
+        self.res_touch_count = (
+            np.full((1, self.window_size - 1), np.nan).flatten().tolist()
+        )
+
+        # Calculate rolling support and resistance touch counts
+        self.dataframe["adj close"].rolling(self.window_size).apply(
+            self._calc_sr,
+            args=(self.dataframe,),
+        )
+
+    def _calc_sr(self, series: pd.Series, dataframe: pd.DataFrame) -> float:
+        """
+        Calculate rolling support and resistance touch counts for a given window.
+
+        :param series: Series which is used for indexing out rolling window.
+        :param dataframe: Original dataframe acting as a source of rolling window.
+        :returns: Dummy float to satisfy Pandas' return value.
+        """
+
+        # Slice out a chunk of dataframe to work with
+        rolling_df = dataframe.loc[series.index]
+
+        # Consider lowest and highest prices
+        # as our support and resistance levels
+        sup_level = rolling_df["adj close"].min()
+        res_level = rolling_df["adj close"].max()
+
+        # Calculate the range between them
+        sup_res_range = res_level - sup_level
+
+        # Calculate tolerance thresholds
+        sup_tolerance = sup_level + self.sup_tolerance * sup_res_range
+        res_tolerance = res_level - self.res_tolerance * sup_res_range
+
+        return 0.0
