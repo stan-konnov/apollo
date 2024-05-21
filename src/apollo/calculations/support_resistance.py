7f6d549637badca9ae487cebd4974891a18af6a8
Stan Konnov <stanis.konnov@gmail.com>
2024-05-21 11:20:40 +0200

    Improving on parameters

--- src/apollo/calculations/support_resistance.py
+++ src/apollo/calculations/support_resistance.py
@@ -8,32 +8,33 @@ class SupportResistanceCalculator(BaseCalculator):
     Support and Resistance calculator.
 
     Calculates rolling support and resistance levels
-    using a parametrized threshold of tolerance.
+    based on threshold of tolerance that defines the
+    levels and number of times they are touched by the price.
     """
 
     def __init__(
         self,
         dataframe: pd.DataFrame,
         window_size: int,
-        sup_count: float,
-        res_count: float,
         sup_tolerance: float,
         res_tolerance: float,
+        sup_touch_count: float,
+        res_touch_count: float,
     ) -> None:
         """
         Construct Support and Resistance calculator.
 
         :param dataframe: Dataframe to calculate support and resistance levels for.
         :param window_size: Window size for rolling support and resistance calculation.
-        :param sup_count: Number of times support level is touched.
-        :param res_count: Number of times resistance level is touched.
         :param sup_tolerance: Tolerance for support level calculation.
         :param res_tolerance: Tolerance for resistance level calculation.
+        :param sup_touch_count: Number of times support level is touched.
+        :param res_touch_count: Number of times resistance level is touched.
         """
 
         super().__init__(dataframe, window_size)
 
-        self.sup_count = sup_count
-        self.res_count = res_count
         self.sup_tolerance = sup_tolerance
         self.res_tolerance = res_tolerance
+        self.sup_touch_count = sup_touch_count
+        self.res_touch_count = res_touch_count
