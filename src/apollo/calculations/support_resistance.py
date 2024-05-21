4ca4629069d0c359f0920dbd4405ad031d7c9798
Stan Konnov <stanis.konnov@gmail.com>
2024-05-21 11:11:10 +0200

    Improving on parameters

--- src/apollo/calculations/support_resistance.py
+++ src/apollo/calculations/support_resistance.py
@@ -15,6 +15,8 @@ class SupportResistanceCalculator(BaseCalculator):
         self,
         dataframe: pd.DataFrame,
         window_size: int,
+        sup_count: float,
+        res_count: float,
         sup_tolerance: float,
         res_tolerance: float,
     ) -> None:
@@ -23,11 +25,15 @@ class SupportResistanceCalculator(BaseCalculator):
 
         :param dataframe: Dataframe to calculate support and resistance levels for.
         :param window_size: Window size for rolling support and resistance calculation.
+        :param sup_count: Number of times support level is touched.
+        :param res_count: Number of times resistance level is touched.
         :param sup_tolerance: Tolerance for support level calculation.
         :param res_tolerance: Tolerance for resistance level calculation.
         """
 
         super().__init__(dataframe, window_size)
 
+        self.sup_count = sup_count
+        self.res_count = res_count
         self.sup_tolerance = sup_tolerance
         self.res_tolerance = res_tolerance
