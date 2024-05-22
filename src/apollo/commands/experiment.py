4fe91600db38e4ef7ada9032938d505865a428b5
Stan Konnov <stanis.konnov@gmail.com>
2024-05-22 21:57:09 +0200

    WIP

--- src/apollo/commands/experiment.py
+++ src/apollo/commands/experiment.py
@@ -1,8 +1,10 @@
 import logging
 
+import pandas as pd
+
 from apollo.api.yahoo_api_connector import YahooApiConnector
-from apollo.calculations.support_resistance_touch_count import (
-    SupportResistanceTouchCountCalculator,
+from apollo.calculations.absolute_price_oscillator import (
+    AbsolutePriceOscillatorCalculator,
 )
 from apollo.settings import END_DATE, START_DATE, TICKER
 
@@ -25,13 +27,17 @@ def main() -> None:
 
     dataframe = yahoo_api_connector.request_or_read_prices()
 
-    sr_calculator = SupportResistanceTouchCountCalculator(
+    apo_calculator = AbsolutePriceOscillatorCalculator(
         dataframe=dataframe,
-        window_size=45,
-        tolerance_threshold=0.2,
+        window_size=5,
+        fast_ema_period=10,
+        slow_ema_period=40,
     )
 
-    sr_calculator.calculate_support_resistance_touch_count()
+    apo_calculator.calculate_absolute_price_oscillator()
+
+    # pd.options.display.max_rows = 10000
+    # print(dataframe)
 
 
 if __name__ == "__main__":
