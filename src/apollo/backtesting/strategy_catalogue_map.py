2509f4c46e65e0267f98fced1bee573d15ed7157
Stan Konnov <stanis.konnov@gmail.com>
2024-05-23 10:49:13 +0200

    Move strategy catalogue map into a separate file

---
+++ src/apollo/backtesting/strategy_catalogue_map.py
@@ -0,0 +1,42 @@
+"""
+Strategy Catalogue Map.
+
+Dictionary that maps strategy names to related classes.
+
+Is used during parameter optimization to instantiate
+the correct strategy class based on the configured name.
+
+Acts as a central registry for all available strategies.
+"""
+
+from apollo.strategies.apo_mean_reversion import AbsolutePriceOscillatorMeanReversion
+from apollo.strategies.arima_trend_mean_reversion import ARIMATrendMeanReversion
+from apollo.strategies.lin_reg_chan_mean_reversion import (
+    LinearRegressionChannelMeanReversion,
+)
+from apollo.strategies.logistic_regression_forecast import LogisticRegressionForecast
+from apollo.strategies.skew_kurt_vol_trend_following import (
+    SkewnessKurtosisVolatilityTrendFollowing,
+)
+from apollo.strategies.support_resistance_trend_following import (
+    SupportResistanceTrendFollowing,
+)
+from apollo.strategies.swing_events_mean_reversion import SwingEventsMeanReversion
+from apollo.utils.types import StrategyCatalogueMap
+
+STRATEGY_CATALOGUE_MAP: StrategyCatalogueMap = {
+    "ARIMATrendMeanReversion":
+        ARIMATrendMeanReversion,
+    "SwingEventsMeanReversion":
+        SwingEventsMeanReversion,
+    "LogisticRegressionForecast":
+        LogisticRegressionForecast,
+    "SupportResistanceTrendFollowing":
+        SupportResistanceTrendFollowing,
+    "LinearRegressionChannelMeanReversion":
+        LinearRegressionChannelMeanReversion,
+    "AbsolutePriceOscillatorMeanReversion":
+        AbsolutePriceOscillatorMeanReversion,
+    "SkewnessKurtosisVolatilityTrendFollowing":
+        SkewnessKurtosisVolatilityTrendFollowing,
+}
