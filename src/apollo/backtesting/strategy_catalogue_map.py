ef8a4aedc64b1a900991ac5c697f8f2b7a447bf6
Stan Konnov <stanis.konnov@gmail.com>
2024-05-27 09:20:35 +0200

    Cleanup APO and SR calculations/strategies as they do not yield acceptable results in light of other strategies

--- src/apollo/backtesting/strategy_catalogue_map.py
+++ src/apollo/backtesting/strategy_catalogue_map.py
@@ -9,7 +9,6 @@ the correct strategy class based on the configured name.
 Acts as a central registry for all available strategies.
 """
 
-from apollo.strategies.apo_mean_reversion import AbsolutePriceOscillatorMeanReversion
 from apollo.strategies.arima_trend_mean_reversion import ARIMATrendMeanReversion
 from apollo.strategies.lin_reg_chan_mean_reversion import (
     LinearRegressionChannelMeanReversion,
@@ -18,9 +17,6 @@ from apollo.strategies.logistic_regression_forecast import LogisticRegressionFor
 from apollo.strategies.skew_kurt_vol_trend_following import (
     SkewnessKurtosisVolatilityTrendFollowing,
 )
-from apollo.strategies.support_resistance_trend_following import (
-    SupportResistanceTrendFollowing,
-)
 from apollo.strategies.swing_events_mean_reversion import SwingEventsMeanReversion
 from apollo.utils.types import StrategyCatalogueMap
 
@@ -28,8 +24,6 @@ STRATEGY_CATALOGUE_MAP: StrategyCatalogueMap = {
     "ARIMATrendMeanReversion": ARIMATrendMeanReversion,
     "SwingEventsMeanReversion": SwingEventsMeanReversion,
     "LogisticRegressionForecast": LogisticRegressionForecast,
-    "SupportResistanceTrendFollowing": SupportResistanceTrendFollowing,
     "LinearRegressionChannelMeanReversion": LinearRegressionChannelMeanReversion,
-    "AbsolutePriceOscillatorMeanReversion": AbsolutePriceOscillatorMeanReversion,
     "SkewnessKurtosisVolatilityTrendFollowing": SkewnessKurtosisVolatilityTrendFollowing,  # noqa: E501
 }
