1a69b7565ee36e483d1772e573788766336701e3
Stan Konnov <stanis.konnov@gmail.com>
2024-05-29 12:39:56 +0200

    BB calculator

--- src/apollo/calculations/bollinger_bands.py
+++ src/apollo/calculations/bollinger_bands.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame
+import numpy as np
+import pandas as pd
 
 from apollo.calculations.base_calculator import BaseCalculator
 
@@ -16,7 +17,7 @@ class BollingerBandsCalculator(BaseCalculator):
 
     def __init__(
         self,
-        dataframe: DataFrame,
+        dataframe: pd.DataFrame,
         window_size: int,
         channel_sd_spread: float,
     ) -> None:
@@ -31,3 +32,56 @@ class BollingerBandsCalculator(BaseCalculator):
         super().__init__(dataframe, window_size)
 
         self.channel_sd_spread = channel_sd_spread
+
+        self.lb_band: list[float] = []
+        self.ub_band: list[float] = []
+
+    def calculate_bollinger_bands(self) -> None:
+        """Calculate Bollinger Bands."""
+
+        # Fill bands arrays with N NaN, where N = window size
+        self.lb_band = np.full((1, self.window_size - 1), np.nan).flatten().tolist()
+        self.ub_band = np.full((1, self.window_size - 1), np.nan).flatten().tolist()
+
+        # Calculate simple moving average to act as the middle band
+        self.dataframe["sma"] = (
+            self.dataframe["adj close"]
+            .rolling(
+                self.window_size,
+            )
+            .mean()
+        )
+
+        # Calculate bands by using SMA and standard deviation
+        self.dataframe["adj close"].rolling(self.window_size).apply(
+            self._calc_bands,
+            args=(self.dataframe,),
+        )
+
+        # Drop SMA from the dataframe
+        self.dataframe.drop(columns="sma", inplace=True)
+
+    def _calc_bands(self, series: pd.Series, dataframe: pd.DataFrame) -> float:
+        """
+        Calculate rolling Bollinger Bands.
+
+        :param series: Series which is used for indexing out rolling window.
+        :param dataframe: Original dataframe acting as a source of rolling window.
+        :returns: Dummy float to satisfy Pandas' return value.
+        """
+
+        # Slice out a chunk of dataframe to work with
+        rolling_df = dataframe.loc[series.index]
+
+        # Calculate standard deviation of adjusted close
+        std = series.std()
+
+        # Calculate lower and upper bands
+        l_band = rolling_df["sma"] - std * self.channel_sd_spread
+        u_band = rolling_df["sma"] + std * self.channel_sd_spread
+
+        self.lb_band.append(l_band[-1])
+        self.ub_band.append(u_band[-1])
+
+        # Return dummy float
+        return 0.0
