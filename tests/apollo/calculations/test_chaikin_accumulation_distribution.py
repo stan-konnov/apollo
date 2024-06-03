49d8d0e0ca23caeeaecb6358d595449e24c16bfc
Stan Konnov <stanis.konnov@gmail.com>
2024-06-03 20:49:28 +0200

    More tests

--- tests/apollo/calculations/test_chaikin_accumulation_distribution.py
+++ tests/apollo/calculations/test_chaikin_accumulation_distribution.py
@@ -25,3 +25,30 @@ def test__calculate_chaikin_accumulation_distribution_line__for_correct_columns(
 
     assert "adl" in cad_calculator.dataframe.columns
     assert "prev_adl" in cad_calculator.dataframe.columns
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_chaikin_accumulation_distribution_line__for_correct_rolling_window(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_chaikin_accumulation_distribution_line for correct rolling window.
+
+    Where N = WINDOW_SIZE.
+
+    Resulting dataframe must skip WINDOW_SIZE - 1 rows for adl column
+    Since Chaikin Accumulation Distribution must have at least N rows to be calculated.
+
+    Resulting dataframe must skip WINDOW_SIZE rows for prev_adl column
+    Since previous AD line skips N + 1 rows due to shifting original AD line.
+    """
+
+    cad_calculator = ChaikinAccumulationDistributionCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+    cad_calculator.calculate_chaikin_accumulation_distribution_line()
+
+    assert dataframe["adl"].isna().sum() == window_size - 1
+    assert dataframe["prev_adl"].isna().sum() == window_size
