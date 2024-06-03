25a827b891538fe838e53248bbc8543c1b67c909
Stan Konnov <stanis.konnov@gmail.com>
2024-06-03 19:47:45 +0200

    Dishing out tests

--- tests/apollo/calculations/test_bollinger_bands.py
+++ tests/apollo/calculations/test_bollinger_bands.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -23,7 +24,6 @@ def test__calculate_bollinger_bands__for_correct_columns(
         dataframe=dataframe,
         window_size=window_size,
     )
-
     mnma_calculator.calculate_mcnicholl_moving_average()
 
     bb_calculator = BollingerBandsCalculator(
@@ -31,7 +31,6 @@ def test__calculate_bollinger_bands__for_correct_columns(
         window_size=window_size,
         channel_sd_spread=CHANNEL_SD_SPREAD,
     )
-
     bb_calculator.calculate_bollinger_bands()
 
     assert "lb_band" in bb_calculator.dataframe.columns
@@ -56,6 +55,59 @@ def test__calculate_bollinger_bands__for_correct_rolling_window(
         dataframe=dataframe,
         window_size=window_size,
     )
+    mnma_calculator.calculate_mcnicholl_moving_average()
+
+    bb_calculator = BollingerBandsCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+        channel_sd_spread=CHANNEL_SD_SPREAD,
+    )
+    bb_calculator.calculate_bollinger_bands()
+
+    assert dataframe["lb_band"].isna().sum() == window_size - 1
+    assert dataframe["ub_band"].isna().sum() == window_size - 1
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_bollinger_bands__for_correct_atr_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_bollinger_bands method for correct Bollinger Bands calculation.
+
+    Resulting lb_band and ub_band columns must have correct values for each row.
+    """
+
+    lb_band = np.full((1, window_size - 1), np.nan).flatten().tolist()
+    ub_band = np.full((1, window_size - 1), np.nan).flatten().tolist()
+
+    control_dataframe = dataframe.copy()
+
+    # Precalculate McNicholl Moving Average
+    mnma_calculator = McNichollMovingAverageCalculator(
+        dataframe=control_dataframe,
+        window_size=window_size,
+    )
+    mnma_calculator.calculate_mcnicholl_moving_average()
+
+    control_dataframe["adj close"].rolling(window_size).apply(
+        mimic_calc_bands,
+        args=(
+            control_dataframe,
+            lb_band,
+            ub_band,
+            CHANNEL_SD_SPREAD,
+        ),
+    )
+
+    control_dataframe["lb_band"] = lb_band
+    control_dataframe["ub_band"] = ub_band
+
+    mnma_calculator = McNichollMovingAverageCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
 
     mnma_calculator.calculate_mcnicholl_moving_average()
 
@@ -67,5 +119,32 @@ def test__calculate_bollinger_bands__for_correct_rolling_window(
 
     bb_calculator.calculate_bollinger_bands()
 
-    assert dataframe["lb_band"].isna().sum() == window_size - 1
-    assert dataframe["ub_band"].isna().sum() == window_size - 1
+    pd.testing.assert_series_equal(dataframe["lb_band"], control_dataframe["lb_band"])
+    pd.testing.assert_series_equal(dataframe["ub_band"], control_dataframe["ub_band"])
+
+
+def mimic_calc_bands(
+    series: pd.Series,
+    dataframe: pd.DataFrame,
+    lb_band: list[float],
+    ub_band: list[float],
+    channel_sd_spread: float,
+) -> float:
+    """
+    Mimicry of Bollinger Bands calculation for testing purposes.
+
+    Please see BollingerBandsCalculator for
+    detailed explanation of Bollinger Bands calculation.
+    """
+
+    rolling_df = dataframe.loc[series.index]
+
+    std = series.std()
+
+    l_band = rolling_df["mnma"] - std * channel_sd_spread
+    u_band = rolling_df["mnma"] + std * channel_sd_spread
+
+    lb_band.append(l_band[-1])
+    ub_band.append(u_band[-1])
+
+    return 0.0
