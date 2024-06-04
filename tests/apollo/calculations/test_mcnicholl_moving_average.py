27a9d233fbdfbd5356aad2bc0015da685d6d83a7
Stan Konnov <stanis.konnov@gmail.com>
2024-06-04 18:49:23 +0200

    Dishing out tests

--- tests/apollo/calculations/test_mcnicholl_moving_average.py
+++ tests/apollo/calculations/test_mcnicholl_moving_average.py
@@ -47,3 +47,52 @@ def test__calculate_mcnicholl_moving_average__for_correct_rolling_window(
     mnma_calculator.calculate_mcnicholl_moving_average()
 
     assert dataframe["mnma"].isna().sum() == window_size - 1
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_mcnicholl_moving_average__for_correct_mnma_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_mcnicholl_moving_average method for correct calculation.
+
+    Resulting "mnma" column must have correct values for each row.
+    """
+
+    control_dataframe = dataframe.copy()
+
+    smoothing_factor = 2 / (window_size + 1)
+
+    simple_moving_average = (
+        control_dataframe["adj close"]
+        .rolling(window=window_size, min_periods=window_size)
+        .mean()
+    )
+
+    weights = (1 - smoothing_factor) ** pd.Series(
+        len(dataframe.index),
+        index=dataframe.index,
+    )
+
+    weights = weights[::-1]
+
+    weighted_close = control_dataframe["adj close"] * smoothing_factor * weights
+
+    close_cumulative_sum = weighted_close[::-1].expanding().sum()[::-1]
+
+    weights_cumulative_sum = weights.expanding().sum()[::-1]
+
+    mcnicholl_ma = close_cumulative_sum / weights_cumulative_sum
+
+    mcnicholl_ma[:window_size] = simple_moving_average[:window_size]
+
+    control_dataframe["mnma"] = mcnicholl_ma
+
+    mnma_calculator = McNichollMovingAverageCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+    mnma_calculator.calculate_mcnicholl_moving_average()
+
+    pd.testing.assert_series_equal(dataframe["mnma"], control_dataframe["mnma"])
