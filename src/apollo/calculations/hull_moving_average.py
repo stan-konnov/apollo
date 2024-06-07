ef2341cd4e715efba61c13ae36a8de612e4f8a96
Stan Konnov <stanis.konnov@gmail.com>
2024-06-07 17:59:52 +0200

    Cleanup

--- src/apollo/calculations/hull_moving_average.py
+++ src/apollo/calculations/hull_moving_average.py
@@ -1,50 +1,7 @@
 import numpy as np
 import pandas as pd
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 from apollo.calculations.base_calculator import BaseCalculator
-
-# It's work in progress
-# ruff: noqa
-
-"""
-Please use HMA instead of McNicholl Moving Average.
-
-Simple Moving Average (SMA): The SMA is the average of a set of prices over a specified
-time period, with equal weight given to each price.
-It's easy to calculate but can lag behind recent price movements.
-
-Exponential Moving Average (EMA): The EMA gives more weight to recent prices,
-making it more responsive to recent price changes compared to the SMA.
-It's calculated by giving more weight to the most recent data points.
-
-Weighted Moving Average (WMA): The WMA assigns weights to each data point,
-with more weight given to recent data points.
-It's similar to the EMA but allows for more customization of the weight distribution.
-
-Triangular Moving Average (TMA): The TMA is a smoothed version of the SMA, where the
-data points are averaged using a triangular weighting scheme.
-It gives more weight to data points closer to the center of the time period.
-
-Variable Moving Average (VMA): The VMA adjusts its sensitivity based on market
-conditions, similar to the KAMA. It can be more responsive during periods of
-high volatility and less responsive during periods of low volatility.
-
-Hull Moving Average (HMA): The HMA attempts to address the issue of lagging behind
-price movements by using the square root of the time period for smoothing.
-It's designed to be more responsive while maintaining smoothness.
-
-Volume Weighted Moving Average (VWMA): The VWMA gives more weight to data points with
-higher trading volume, reflecting the importance of volume in price movements.
-
-Arithmetic Moving Average (AMA): The AMA adjusts the smoothing factor dynamically
-based on market conditions, aiming to reduce lag during trending markets
-while smoothing out noise during ranging markets.
-
-Adaptive Moving Average (Adaptive MA): Similar to the AMA, the Adaptive MA adjusts its
-sensitivity based on market conditions,
-aiming to adapt to changes in volatility and trend.
-"""
 
 
 class HullMovingAverageCalculator(BaseCalculator):
@@ -62,47 +19,62 @@ class HullMovingAverageCalculator(BaseCalculator):
     def calculate_hull_moving_average(self) -> None:
         """Calculate Hull Moving Average."""
 
-        # DEMA!
-        # Calculate initial SMA
-        # ema_1 = (
-        #     self.dataframe["adj close"]
-        #     .ewm(
-        #         alpha=1 / self.window_size,
-        #         min_periods=self.window_size,
-        #         adjust=False,
-        #     )
-        #     .mean()
-        # )
+        # Calculate weighed moving average of
+        # the close prices using provided window size
+        standard_window_wma = self._calculate_weighted_moving_average(
+            self.dataframe["adj close"],
+            self.window_size,
+        )
 
-        # ema_2 = ema_1.ewm(
-        #     alpha=1 / self.window_size,
-        #     min_periods=self.window_size,
-        #     adjust=False,
-        # ).mean()
-
-        # dema = 2 * ema_1 - ema_2
-
-        # HMA!
-        def wma(data: pd.Series, window: int) -> pd.Series:
-            weights = np.arange(1, window + 1)
-
-            wma_values = data.rolling(window=window).apply(
-                lambda x: np.sum(x * weights) / np.sum(weights), raw=True
-            )
-
-            return wma_values
-
+        # Define a half window size
+        # rounded down to the nearest integer
         half_window = self.window_size // 2
 
-        # Step 1: Calculate WMA of data and half-window
-        wma_data = wma(self.dataframe["adj close"], self.window_size)
-        wma_half = wma(self.dataframe["adj close"], half_window)
+        # Calculate shorter weighed moving average
+        # using half window size
+        half_window_wma = self._calculate_weighted_moving_average(
+            self.dataframe["adj close"],
+            half_window,
+        )
 
-        # Step 2: Calculate difference
-        diff = 2 * wma_half - wma_data
+        # Calculate the difference standard weighted moving average
+        # and the shorter weighted moving average multiplied by 2
+        # to emphasize shorter-term price movements
+        wma_difference = 2 * half_window_wma - standard_window_wma
 
-        # Step 3: Calculate WMA of the difference with square root of window
+        # Take a square root of the window size
+        # to make the Hull Moving Average even more responsive
         sqrt_window = int(np.sqrt(self.window_size))
-        hma = wma(diff, sqrt_window)
 
-        self.dataframe["hma"] = hma
+        # Finally, calculate Hull Moving Average
+        # over the difference using square root of the window size
+        hull_moving_average = self._calculate_weighted_moving_average(
+            wma_difference,
+            sqrt_window,
+        )
+
+        # Write to the dataframe
+        self.dataframe["hma"] = hull_moving_average
+
+    def _calculate_weighted_moving_average(
+        self,
+        series: pd.Series,
+        window_size: int,
+    ) -> pd.Series:
+        """
+        Calculate Weighted Moving Average.
+
+        :param series: Series to calculate WMA for.
+        :returns: Weighted moving average series.
+        """
+
+        # First, calculate range of equal weights (1, 2, ..., N)
+        weights = np.arange(1, window_size + 1)
+
+        # Then, calculate and return weighted moving average
+        # by summing up the product of weights and series
+        # and dividing it by sum of weights
+        return series.rolling(window=window_size).apply(
+            lambda x: np.sum(x * weights) / np.sum(weights),
+            raw=True,
+        )
