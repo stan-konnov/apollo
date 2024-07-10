ce7760aec8f17249b5397ecf4904d3e6f5a9b987
Stan Konnov <stanis.konnov@gmail.com>
2024-07-10 15:45:35 +0200

    Braining out tests

--- tests/apollo/utils/test_common.py
+++ tests/apollo/utils/test_common.py
@@ -1,3 +1,4 @@
+import pytest
 from numpy import datetime64
 
 from apollo.utils.common import ensure_environment_is_configured, to_default_date_string
@@ -13,13 +14,27 @@ def test__to_default_date_string__with_proper_inputs() -> None:
     assert to_default_date_string(datetime64("2021-01-01")) == "2021-01-01"
 
 
-def test__ensure_environment_is_configured__for_correctly_checking_env_variables() -> (
-    None
-):
+def test__ensure_environment_is_configured__for_correctly_checking_env_variables(
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
     """
     Test ensure_environment_is_configured for properly checking env variables.
 
     Function must raise an exception if any of the required variables are not set.
     """
 
-    ensure_environment_is_configured()
+    monkeypatch.setenv("TICKER", "")
+
+    exception_message = str(
+        "TICKER, EXCHANGE, STRATEGY, START_DATE, END_DATE, "
+        "INFLUXDB_BUCKET, INFLUXDB_ORG, INFLUXDB_TOKEN, INFLUXDB_URL "
+        "environment variables must be set.",
+    )
+
+    with pytest.raises(
+        ValueError,
+        match=exception_message,
+    ) as exception:
+        ensure_environment_is_configured()
+
+    assert str(exception.value) == exception_message
