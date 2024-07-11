6494739d1f6c3b3549f6abd0facc94d2ba994f1a
Stan Konnov <stanis.konnov@gmail.com>
2024-07-11 17:24:17 +0200

    Dishing out tests for data availability helper

---
+++ tests/apollo/utils/test_data_availability_helper.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from freezegun import freeze_time
+from zoneinfo import ZoneInfo
+
+from apollo.utils.data_availability_helper import DataAvailabilityHelper
+
+
+# Assume today date is Friday, 2024-07-12
+@freeze_time("2024-07-12")
+def test__check_if_price_data_needs_update__with_last_record_before_prev_b_day() -> (
+    None
+):
+    """
+    Test check_if_price_data_needs_update with last record before previous business day.
+
+    Function should return True.
+    """
+
+    # Assume last available record date is Monday, 2024-07-01
+    last_record_date = datetime(2024, 7, 1, tzinfo=ZoneInfo("UTC")).date()
+
+    result = DataAvailabilityHelper.check_if_price_data_needs_update(
+        last_record_date,
+    )
+
+    assert result is True
