74207320d0ea967b91a522fdaa197c1ee9e39fdf
Stan Konnov <stanis.konnov@gmail.com>
2024-07-18 11:24:46 +0200

    Improving environment checks

--- src/apollo/utils/common.py
+++ src/apollo/utils/common.py
@@ -1,10 +1,12 @@
 from numpy import datetime64
 from pandas import to_datetime
 
+from apollo.backtesting.strategy_catalogue_map import STRATEGY_CATALOGUE_MAP
 from apollo.settings import (
     DEFAULT_DATE_FORMAT,
     END_DATE,
     EXCHANGE,
+    EXCHANGE_TIME_ZONE_AND_HOURS,
     FREQUENCY,
     INFLUXDB_BUCKET,
     INFLUXDB_MEASUREMENT,
@@ -15,6 +17,7 @@ from apollo.settings import (
     START_DATE,
     STRATEGY,
     TICKER,
+    YahooApiFrequencies,
 )
 
 
@@ -38,6 +41,9 @@ def ensure_environment_is_configured() -> None:
     Is run at the start of every command.
 
     :raises ValueError: If any of the environment variables are not configured.
+    :raises ValueError: If the strategy is not a valid strategy.
+    :raises ValueError: If the exchange is not a valid exchange.
+    :raises ValueError: If the frequency is not a valid frequency.
     """
 
     required_variables = {
@@ -55,7 +61,29 @@ def ensure_environment_is_configured() -> None:
         "INFLUXDB_MEASUREMENT": INFLUXDB_MEASUREMENT,
     }
 
+    # Check if any of the required variables are not set
     if any(not variable for variable in required_variables.values()):
         raise ValueError(
             f"{', '.join(required_variables)} environment variables must be set.",
+        )
+
+    # Check if strategy is a valid strategy
+    if STRATEGY not in STRATEGY_CATALOGUE_MAP:
+        raise ValueError(
+            f"Invalid STRATEGY environment variable: {STRATEGY}. "
+            f"Accepted values: {', '.join(STRATEGY_CATALOGUE_MAP)}",
+        )
+
+    # Check if exchange is a valid exchange
+    if EXCHANGE not in EXCHANGE_TIME_ZONE_AND_HOURS:
+        raise ValueError(
+            f"Invalid EXCHANGE environment variable: {EXCHANGE}. "
+            f"Accepted values: {', '.join(EXCHANGE_TIME_ZONE_AND_HOURS)}",
+        )
+
+    # Check if the frequency is a valid Yahoo Finance API frequency
+    if FREQUENCY not in YahooApiFrequencies:
+        raise ValueError(
+            f"Invalid FREQUENCY environment variable: {FREQUENCY}. "
+            f"Accepted values: {', '.join([f.value for f in YahooApiFrequencies])}",
         )
