4e6945642b2470cb6584f25d65c573997d11a12d
Stan Konnov <stanis.konnov@gmail.com>
2024-07-19 10:03:54 +0200

    Further work on tests

--- tests/apollo/utils/test_common.py
+++ tests/apollo/utils/test_common.py
@@ -7,6 +7,7 @@ from apollo.backtesting.strategy_catalogue_map import STRATEGY_CATALOGUE_MAP
 from apollo.settings import (
     END_DATE,
     EXCHANGE,
+    EXCHANGE_TIME_ZONE_AND_HOURS,
     FREQUENCY,
     INFLUXDB_BUCKET,
     INFLUXDB_MEASUREMENT,
@@ -82,6 +83,28 @@ def test__ensure_environment_is_configured__for_invalidating_strategy() -> None:
     exception_message = str(
         "Invalid STRATEGY environment variable: SomeNonExistentStrategy. "
         f"Accepted values: {', '.join(STRATEGY_CATALOGUE_MAP)}",
+    )
+
+    with pytest.raises(
+        ValueError,
+        match=exception_message,
+    ) as exception:
+        ensure_environment_is_configured()
+
+    assert str(exception.value) == exception_message
+
+
+@patch("apollo.utils.common.EXCHANGE", "ABCD")
+def test__ensure_environment_is_configured__for_invalidating_exchange() -> None:
+    """
+    Test ensure_environment_is_configured for invalidating exchange.
+
+    Function must raise an exception if the exchange is not a valid exchange.
+    """
+
+    exception_message = str(
+        "Invalid EXCHANGE environment variable: ABCD. "
+        f"Accepted values: {', '.join(EXCHANGE_TIME_ZONE_AND_HOURS)}",
     )
 
     with pytest.raises(
