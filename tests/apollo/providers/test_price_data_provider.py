64d9482bb8488914c8a580fe47e515eaa0956d5f
Stan Konnov <stanis.konnov@gmail.com>
2024-07-31 20:31:40 +0200

    More unit tests for price data provider

--- tests/apollo/providers/test_price_data_provider.py
+++ tests/apollo/providers/test_price_data_provider.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from unittest.mock import Mock
 from zoneinfo import ZoneInfo
 
+from freezegun import freeze_time
 import pandas as pd
 import pytest
 
@@ -242,126 +243,86 @@ def test__get_price_data__with_valid_parameters_and_data_present_to_refresh() ->
     pd.testing.assert_frame_equal(price_dataframe, expected_dataframe_to_write)
 
 
-# @pytest.mark.usefixtures("yahoo_api_response")
-# def test__request_or_read_prices__with_max_period() -> None:
-#     """
-#     Test request_or_read_prices method with max_period.
+@freeze_time(f"{END_DATE} 17:00:00")
+def test__get_price_data__with_valid_parameters_and_intraday_data() -> None:
+    """
+    Test get_price_data method with valid parameters.
 
-#     API Connector must construct request arguments with period=max.
-#     API Connector must call InfluxDB connector to get last record date.
-#     API Connector must call InfluxDB connector without start and end date.
-#     """
+    And requested data is intraday.
 
-#     api_connector = YahooApiConnector(
-#         ticker=str(TICKER),
-#         start_date=str(START_DATE),
-#         end_date=str(END_DATE),
-#         max_period=True,
-#     )
+    Data Provider must call InfluxDB connector to get last record date.
+    Data Provider must parse last queried date.
+    Data Provider must call API connector to request price data.
+    Data Provider must call InfluxDB connector to write price data without intraday.
+    Data Provider must return a pandas Dataframe with price data.
+    """
 
-#     api_connector._database_connector = Mock(InfluxDbConnector)
-#     api_connector._database_connector.get_last_record_date.return_value = datetime.now(
-#         tz=ZoneInfo("UTC"),
-#     ).date()
+    price_data_provider = PriceDataProvider(
+        ticker=str(TICKER),
+        frequency=str(FREQUENCY),
+        start_date=str(START_DATE),
+        end_date=str(END_DATE),
+        max_period=bool(MAX_PERIOD),
+    )
 
-#     api_connector.request_or_read_prices()
+    price_data_provider._api_connector = Mock(YahooApiConnector)  # noqa: SLF001
+    price_data_provider._api_connector.request_price_data.return_value = (  # noqa: SLF001
+        # We copy the dataframe to avoid
+        # modifying the inputs between tests
+        API_RESPONSE_DATAFRAME.copy()
+    )
 
-#     api_connector._database_connector.get_last_record_date.assert_called_once()
-#     api_connector._database_connector.read_price_data.assert_called_once_with(
-#         ticker=TICKER,
-#         frequency=api_connector._frequency,
-#     )
+    price_data_provider._database_connector = Mock(InfluxDbConnector)
+    price_data_provider._database_connector.get_last_record_date.return_value = None
 
-#     assert api_connector._request_arguments["period"] == "max"
+    # We copy the dataframe to avoid
+    # modifying the inputs between tests
+    expected_dataframe_to_write = API_RESPONSE_DATAFRAME.copy()
 
+    expected_dataframe_to_write.reset_index(inplace=True)
+    expected_dataframe_to_write.columns = (
+        expected_dataframe_to_write.columns.str.lower()
+    )
+    expected_dataframe_to_write.set_index("date", inplace=True)
+    expected_dataframe_to_write.insert(0, "ticker", TICKER)
 
-# @pytest.mark.usefixtures("yahoo_api_response")
-# def test__request_or_read_prices__with_start_and_end_date() -> None:
-#     """
-#     Test request_or_read_prices method with start and end date.
+    expected_dataframe_to_write.drop(
+        index=expected_dataframe_to_write.index[-1],
+        inplace=True,
+    )
 
-#     API Connector must call InfluxDB connector to get last record date.
-#     API Connector must call InfluxDB connector with start and end date.
-#     API Connector must construct request arguments with start and end date.
-#     """
+    adjustment_factor = (
+        expected_dataframe_to_write["adj close"] / expected_dataframe_to_write["close"]
+    )
 
-#     api_connector = YahooApiConnector(
-#         ticker=str(TICKER),
-#         start_date=str(START_DATE),
-#         end_date=str(END_DATE),
-#         max_period=False,
-#     )
+    for column in ["open", "high", "low", "volume"]:
+        expected_dataframe_to_write[f"adj {column}"] = (
+            expected_dataframe_to_write[column] * adjustment_factor
+        )
 
-#     api_connector._database_connector = Mock(InfluxDbConnector)
-#     api_connector._database_connector.get_last_record_date.return_value = datetime.now(
-#         tz=ZoneInfo("UTC"),
-#     ).date()
+    price_dataframe = price_data_provider.get_price_data()
 
-#     api_connector.request_or_read_prices()
+    price_data_provider._database_connector.get_last_record_date.assert_called_once_with(  # noqa: SLF001
+        ticker=str(TICKER),
+        frequency=str(FREQUENCY),
+    )
 
-#     api_connector._database_connector.get_last_record_date.assert_called_once()
-#     api_connector._database_connector.read_price_data.assert_called_once_with(
-#         ticker=TICKER,
-#         frequency=api_connector._frequency,
-#         start_date=START_DATE,
-#         end_date=END_DATE,
-#     )
+    price_data_provider._api_connector.request_price_data.assert_called_once_with(
+        ticker=str(TICKER),
+        frequency=str(FREQUENCY),
+        start_date=str(START_DATE),
+        end_date=str(END_DATE),
+        max_period=bool(MAX_PERIOD),
+    )
 
-#     assert api_connector._request_arguments["start"] == START_DATE
-#     assert api_connector._request_arguments["end"] == END_DATE
+    price_data_provider._database_connector.write_price_data.assert_called_once_with(  # noqa: SLF001
+        frequency=str(FREQUENCY),
+        # Please see tests/fixtures/window_size_and_dataframe.py
+        # for explanation on SameDataframe class
+        dataframe=SameDataframe(expected_dataframe_to_write),
+    )
 
-
-# @freeze_time(f"{END_DATE} 17:00:00")
-# @pytest.mark.usefixtures("yahoo_api_response")
-# def test__request_or_read_prices__with_valid_parameters_and_intraday_data() -> None:
-#     """
-#     Test request_or_read_prices method with valid parameters.
-
-#     And requested data is intraday.
-
-#     API Connector must parse last queried date.
-
-#     API Connector must call InfluxDB connector to get last record date.
-#     API Connector must call InfluxDB connector to write price data without intraday.
-#     API Connector must return a pandas Dataframe with price data.
-#     """
-
-#     api_connector = YahooApiConnector(
-#         ticker=str(TICKER),
-#         start_date=str(START_DATE),
-#         end_date=str(END_DATE),
-#     )
-
-#     api_connector._database_connector = Mock(InfluxDbConnector)
-#     api_connector._database_connector.get_last_record_date.return_value = None
-
-#     expected_dataframe_to_write = API_RESPONSE_DATAFRAME.copy()
-
-#     expected_dataframe_to_write.reset_index(inplace=True)
-#     expected_dataframe_to_write.drop(columns="index", inplace=True)
-#     expected_dataframe_to_write.columns = (
-#         expected_dataframe_to_write.columns.str.lower()
-#     )
-
-#     expected_dataframe_to_write.set_index("date", inplace=True)
-#     expected_dataframe_to_write.insert(0, "ticker", TICKER)
-
-#     expected_dataframe_to_write.drop(
-#         index=expected_dataframe_to_write.index[-1],
-#         inplace=True,
-#     )
-
-#     price_dataframe = api_connector.request_or_read_prices()
-
-#     api_connector._database_connector.get_last_record_date.assert_called_once()
-#     api_connector._database_connector.write_price_data.assert_called_once_with(
-#         frequency=api_connector._frequency,
-#         # Please see tests/fixtures/window_size_and_dataframe.py
-#         # for explanation on SameDataframe class
-#         dataframe=SameDataframe(expected_dataframe_to_write),
-#     )
-
-#     pd.testing.assert_frame_equal(price_dataframe, expected_dataframe_to_write)
+    pd.testing.assert_frame_equal(price_dataframe, expected_dataframe_to_write)
 
 
 def test__price_data_provider__with_invalid_date_format() -> None:
