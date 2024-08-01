b19f8db09c5489de5c280a00c7582bfcb736dd69
Stan Konnov <stanis.konnov@gmail.com>
2024-08-01 22:09:14 +0200

    More scrutiny

--- tests/apollo/calculations/test_wilders_swing_index.py
+++ tests/apollo/calculations/test_wilders_swing_index.py
@@ -53,3 +53,32 @@ def test__calculate_swing_index__for_correct_rolling_window(
     wsi_calculator.calculate_swing_index()
 
     assert dataframe["sp"].isna().sum() == window_size
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calc_wtr__with_invalid_base_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calc_wtr method with invalid base calculation.
+
+    The provided value for calculation of Weighted True Range is invalid.
+
+    WildersSwingIndexCalculator must raise ValueError if base calculation is invalid.
+    """
+
+    wsi_calculator = WildersSwingIndexCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+
+    exception_message = "Provided diff_index is invalid. Base calculation is faulty."
+
+    with pytest.raises(
+        ValueError,
+        match=exception_message,
+    ) as exception:
+        wsi_calculator._calc_wtr(999, 1, 1, 1, 1)  # noqa: SLF001
+
+    assert str(exception.value) == exception_message
