c0f89b38b2559b1e9d03cf95b0ff1e4aa7f40eaf
Stan Konnov <stanis.konnov@gmail.com>
2024-08-13 21:25:42 +0200

    More backtesting; we're actually buying expanding VIX

--- src/apollo/commands/backtest.py
+++ src/apollo/commands/backtest.py
@@ -10,8 +10,8 @@ from apollo.settings import (
     TICKER,
     VIX_TICKER,
 )
-from apollo.strategies.skew_kurt_vol_trend_following import (
-    SkewnessKurtosisVolatilityTrendFollowing,
+from apollo.strategies.lin_reg_chan_mean_reversion import (
+    LinearRegressionChannelMeanReversion,
 )
 from apollo.utils.common import ensure_environment_is_configured
 
@@ -51,21 +51,20 @@ def main() -> None:
     dataframe["vix open"] = vix_dataframe["open"]
     dataframe["vix close"] = vix_dataframe["close"]
 
-    strategy = SkewnessKurtosisVolatilityTrendFollowing(
+    strategy = LinearRegressionChannelMeanReversion(
         dataframe=dataframe,
-        window_size=20,
-        kurtosis_threshold=0.5,
-        volatility_multiplier=1.0,
+        window_size=5,
+        channel_sd_spread=0.8,
     )
 
     strategy.model_trading_signals()
 
     backtesting_runner = BacktestingRunner(
         dataframe=dataframe,
-        strategy_name="SkewnessKurtosisVolatilityTrendFollowing",
+        strategy_name="LinearRegressionChannelMeanReversion",
         lot_size_cash=1000,
         sl_volatility_multiplier=0.1,
-        tp_volatility_multiplier=0.4,
+        tp_volatility_multiplier=0.3,
         write_result_plot=True,
     )
 
