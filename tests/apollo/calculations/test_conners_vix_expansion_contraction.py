6e7c800798b3ba12df4546ac98c9e049a50ab71f
Stan Konnov <stanis.konnov@gmail.com>
2024-08-22 19:36:57 +0200

    Dishing out tests for calculator

---
+++ tests/apollo/calculations/test_conners_vix_expansion_contraction.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pytest
+
+from apollo.calculations.conners_vix_expansion_contraction import (
+    ConnersVixExpansionContractionCalculator,
+)
+
+
+@pytest.mark.usefixtures("enhanced_dataframe", "window_size")
+def test__calculate_vix_expansion_contraction__for_correct_columns(
+    enhanced_dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_vix_expansion_contraction method for correct columns.
+
+    Resulting dataframe must have "cvec" column.
+    Resulting dataframe must drop "vix_prev_open" and "vix_prev_close" columns.
+    """
+
+    cvec_calculator = ConnersVixExpansionContractionCalculator(
+        dataframe=enhanced_dataframe,
+        window_size=window_size,
+    )
+
+    cvec_calculator.calculate_vix_expansion_contraction()
+
+    assert "cvec" in enhanced_dataframe.columns
+    assert "vix_prev_open" not in enhanced_dataframe.columns
+    assert "vix_prev_close" not in enhanced_dataframe.columns
