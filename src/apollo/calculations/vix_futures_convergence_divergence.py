1dfd22d90407afbbd7e22c92b61c9ce0d98030e0
Stan Konnov <stanis.konnov@gmail.com>
2024-08-28 18:41:27 +0200

    Some setup and comments

---
+++ src/apollo/calculations/vix_futures_convergence_divergence.py
@@ -0,0 +1,11 @@
+from apollo.calculations.base_calculator import BaseCalculator
+
+
+class VixFutureConvergenceDivergenceCalculator(BaseCalculator):
+    """
+    VIX Futures Convergence Divergence Calculator.
+
+    WIP: investigation needed to document.
+
+    Calculates percentage change disparity between VIX and S&P 500 Futures
+    """
