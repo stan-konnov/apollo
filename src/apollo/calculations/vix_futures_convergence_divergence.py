5115657e2ab530e19b70aef0fb661db2f1bd3487
Stan Konnov <stanis.konnov@gmail.com>
2024-08-29 17:59:29 +0200

    Isolate VIX and SP500 Futures percentage change calculations into a calculator

--- src/apollo/calculations/vix_futures_convergence_divergence.py
+++ src/apollo/calculations/vix_futures_convergence_divergence.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from apollo.calculations.base_calculator import BaseCalculator
 
 
@@ -5,7 +7,40 @@ class VixFutureConvergenceDivergenceCalculator(BaseCalculator):
     """
     VIX Futures Convergence Divergence Calculator.
 
-    Calculates percentage change disparity between VIX and S&P 500 Futures.
-
-    Operates
+    Calculates percentage change difference between VIX and S&P 500 Futures.
+    Captures increasing/decreasing convergence and divergence between the two.
     """
+
+    def __init__(self, dataframe: pd.DataFrame, window_size: int) -> None:
+        """
+        Initialize the VIX Futures Convergence Divergence Calculator.
+
+        :param dataframe: Dataframe to calculate Convergence Divergence for.
+        :param window_size: Size of the window for Convergence Divergence calculation.
+
+        NOTE: even though we accept window_size parameter,
+        calculator does not perform any rolling calculations.
+        """
+        super().__init__(dataframe, window_size)
+
+    def calculate_convergence_divergence(self) -> None:
+        """Calculate VIX Futures Convergence Divergence."""
+
+        # Get previous close price
+        # Is a necessary for strategies revolving
+        # around VIX and S&P 500 Futures percentage change
+        self._dataframe["prev_close"] = self._dataframe["adj close"].shift(1)
+
+        # Calculate percentage change for VIX and S&P 500 Futures
+        self._dataframe["vix_pct_change"] = self._dataframe["vix close"].pct_change()
+        self._dataframe["spf_pct_change"] = self._dataframe["spf close"].pct_change()
+
+        # Calculate percentage change difference between VIX and S&P 500 Futures
+        self._dataframe["vix_spf_pct_change_diff"] = (
+            self._dataframe["vix_pct_change"] - self._dataframe["spf_pct_change"]
+        )
+
+        # Shift to get previous percentage change difference
+        self._dataframe["prev_vix_spf_pct_change_diff"] = self._dataframe[
+            "vix_spf_pct_change_diff"
+        ].shift(1)
