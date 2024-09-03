1a6f609484a7e9d46df51baee9043738c9c05606
Stan Konnov <stanis.konnov@gmail.com>
2024-09-03 21:13:05 +0200

    Experiments, experiments, experiments

--- src/apollo/calculations/vix_futures_convergence_divergence.py
+++ src/apollo/calculations/vix_futures_convergence_divergence.py
@@ -26,14 +26,36 @@ class VixFuturesConvergenceDivergenceCalculator(BaseCalculator):
     def calculate_vix_futures_convergence_divergence(self) -> None:
         """Calculate VIX Futures Convergence Divergence."""
 
-        # Calculate percentage change for VIX and S&P 500 Futures
-        self._dataframe["vix_pct_change"] = self._dataframe["vix close"].pct_change()
-        self._dataframe["spf_pct_change"] = self._dataframe["spf close"].pct_change()
+        # Since we are working with multiple
+        # data sources, there is no guarantee that
+        # the data is present for all the rows in the dataframe
+        # We, therefore, need to check against NaNs after calculation,
+        # since calculating over missing data results in NaNs that are dropped
 
-        # Calculate percentage change difference between VIX and S&P 500 Futures
-        self._dataframe["vix_spf_pct_diff"] = (
-            self._dataframe["vix_pct_change"] - self._dataframe["spf_pct_change"]
-        )
+        # Initialize necessary columns with 0s
+        self._dataframe["vix_pct_change"] = 0
+        self._dataframe["spf_pct_change"] = 0
+        self._dataframe["vix_spf_pct_diff"] = 0
+
+        # Calculate percentage change for VIX and
+        # S&P 500 Futures only if the data is present
+        self._dataframe.loc[
+            self._dataframe["vix close"] != 0,
+            "vix_pct_change",
+        ] = self._dataframe["vix close"].pct_change()
+
+        self._dataframe.loc[
+            self._dataframe["spf close"] != 0,
+            "spf_pct_change",
+        ] = self._dataframe["spf close"].pct_change()
+
+        # Calculate percentage change difference
+        # between VIX and S&P 500 Futures only if the data is present
+        self._dataframe.loc[
+            self._dataframe["vix_pct_change"].notna()
+            & self._dataframe["spf_pct_change"].notna(),
+            "vix_spf_pct_diff",
+        ] = self._dataframe["vix_pct_change"] - self._dataframe["spf_pct_change"]
 
         # Shift to get previous percentage change difference
         self._dataframe["prev_vix_spf_pct_diff"] = self._dataframe[
--- src/apollo/commands/backtest.py
+++ src/apollo/commands/backtest.py
@@ -10,8 +10,8 @@ from apollo.settings import (
     START_DATE,
     TICKER,
 )
-from apollo.strategies.vix_exp_con_mean_reversion import (
-    VIXExpansionContractionMeanReversion,
+from apollo.strategies.vix_fut_con_div_trend_following import (
+    VIXFuturesConvergenceDivergenceTrendFollowing,
 )
 from apollo.utils.common import ensure_environment_is_configured
 
@@ -44,16 +44,16 @@ def main() -> None:
         additional_data_enhancers=["VIX", "SP500 Futures"],
     )
 
-    strategy = VIXExpansionContractionMeanReversion(
+    strategy = VIXFuturesConvergenceDivergenceTrendFollowing(
         dataframe=dataframe,
-        window_size=10,
+        window_size=20,
     )
 
     strategy.model_trading_signals()
 
     backtesting_runner = BacktestingRunner(
         dataframe=dataframe,
-        strategy_name="VIXExpansionContractionMeanReversion",
+        strategy_name="VIXFuturesConvergenceDivergenceTrendFollowing",
         lot_size_cash=1000,
         sl_volatility_multiplier=0.1,
         tp_volatility_multiplier=0.4,
