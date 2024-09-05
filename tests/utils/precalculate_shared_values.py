1371915c1b96a72db9950a73a836731eedf09f6c
Stan Konnov <stanis.konnov@gmail.com>
2024-09-06 00:04:02 +0200

    Docstrings

--- tests/utils/precalculate_shared_values.py
+++ tests/utils/precalculate_shared_values.py
@@ -5,8 +5,8 @@ def precalculate_shared_values(dataframe: pd.DataFrame) -> pd.DataFrame:
     """
     Precalculate shared values for the dataframe.
 
-    :param dataframe: The dataframe to precalculate shared values for.
-    :returns: The dataframe with precalculated shared values.
+    :param dataframe: Dataframe to precalculate shared values for.
+    :returns: Dataframe with precalculated shared values.
     """
 
     dataframe["prev_close"] = dataframe["adj close"].shift(1)
