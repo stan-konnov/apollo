b29e3fc903c7cd8a508f6ffe886f1164fa8f0565
Stan Konnov <stanis.konnov@gmail.com>
2024-09-09 19:22:04 +0200

    Dishing out unit tests for new calculators

--- tests/apollo/calculations/test_engulfing_vix_pattern.py
+++ tests/apollo/calculations/test_engulfing_vix_pattern.py
@@ -4,10 +4,11 @@ import pytest
 from apollo.calculations.engulfing_vix_pattern import (
     EngulfingVIXPatternCalculator,
 )
+from apollo.settings import MISSING_DATA_PLACEHOLDER
 
-UPSIDE_EXPANSION: float = 1.0
-DOWNSIDE_CONTRACTION: float = -1.0
-NO_SIGNIFICANT_MOVEMENT: float = 0.0
+NO_PATTERN: float = 0.0
+BULLISH_ENGULFING: float = 1.0
+BEARISH_ENGULFING: float = -1.0
 
 """
 TODO: All new calculators should test for not calculating over missing data
@@ -39,64 +40,64 @@ def test__calculate_engulfing_vix_pattern__for_correct_columns(
 
 
 @pytest.mark.usefixtures("enhanced_dataframe", "window_size")
-def test__calculate_vix_expansion_contraction__for_correct_cvec_calculation(
+def test__calculate_engulfing_vix_pattern__for_correct_cvec_calculation(
     enhanced_dataframe: pd.DataFrame,
     window_size: int,
 ) -> None:
     """
-    Test calculate_vix_expansion_contraction method for correct Expansion Contraction.
+    Test calculate_engulfing_vix_pattern method for correct Engulfing Pattern.
 
-    Resulting "cvec" column must have correct values for each row.
+    Resulting "vixep" column must have correct values for each row.
     """
 
     control_dataframe = enhanced_dataframe.copy()
 
-    control_dataframe["vix_prev_open"] = control_dataframe["vix open"].shift(1)
-    control_dataframe["vix_prev_close"] = control_dataframe["vix close"].shift(1)
+    control_dataframe["vixep"] = NO_PATTERN
 
-    control_dataframe["cvec"] = (
-        control_dataframe["adj close"]
-        .rolling(
-            window_size,
-        )
-        .apply(
-            mimic_calc_cvec,
-            args=(control_dataframe,),
-        )
+    control_dataframe["vix_prev_open"] = 0.0
+    control_dataframe["vix_prev_close"] = 0.0
+
+    control_dataframe.loc[
+        control_dataframe["vix open"] != MISSING_DATA_PLACEHOLDER,
+        "vix_prev_open",
+    ] = control_dataframe["vix open"].shift(1)
+
+    control_dataframe.loc[
+        control_dataframe["vix close"] != MISSING_DATA_PLACEHOLDER,
+        "vix_prev_close",
+    ] = control_dataframe["vix close"].shift(1)
+
+    control_dataframe.loc[
+        (
+            (control_dataframe["vix open"] < control_dataframe["vix_prev_open"])
+            & (control_dataframe["vix close"] > control_dataframe["vix_prev_close"])
+            & (control_dataframe["vix close"] > control_dataframe["vix open"])
+        ),
+        "vixep",
+    ] = BULLISH_ENGULFING
+
+    control_dataframe.loc[
+        (
+            (control_dataframe["vix open"] > control_dataframe["vix_prev_open"])
+            & (control_dataframe["vix close"] < control_dataframe["vix_prev_close"])
+            & (control_dataframe["vix close"] < control_dataframe["vix open"])
+        ),
+        "vixep",
+    ] = BEARISH_ENGULFING
+
+    control_dataframe.drop(
+        columns=["vix_prev_open", "vix_prev_close"],
+        inplace=True,
     )
 
-    cvec_calculator = EngulfingVIXPatternCalculator(
+    evp_calculator = EngulfingVIXPatternCalculator(
         dataframe=enhanced_dataframe,
         window_size=window_size,
     )
 
-    cvec_calculator.calculate_vix_expansion_contraction()
+    evp_calculator.calculate_engulfing_vix_pattern()
 
     pd.testing.assert_series_equal(
-        control_dataframe["cvec"],
-        enhanced_dataframe["cvec"],
+        control_dataframe["vixep"],
+        enhanced_dataframe["vixep"],
     )
-
-
-def mimic_calc_cvec(series: pd.Series, dataframe: pd.DataFrame) -> float:
-    """
-    Mimicry of Expansion Contraction calculation for testing purposes.
-
-    Please see EngulfingVIXPatternCalculator for detailed explanation.
-    """
-
-    rolling_df = dataframe.loc[series.index]
-
-    curr_open = rolling_df["vix open"].iloc[-1]
-    curr_close = rolling_df["vix close"].iloc[-1]
-
-    prev_open = rolling_df["vix_prev_open"].iloc[-1]
-    prev_close = rolling_df["vix_prev_close"].iloc[-1]
-
-    if curr_open < prev_open and curr_close > prev_close and curr_close > curr_open:
-        return UPSIDE_EXPANSION
-
-    if curr_open > prev_open and curr_close < prev_close and curr_close < curr_open:
-        return DOWNSIDE_CONTRACTION
-
-    return NO_SIGNIFICANT_MOVEMENT
