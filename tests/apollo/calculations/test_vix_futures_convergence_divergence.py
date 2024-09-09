ff818a1355af1e1ab686f35d64858e065fafb913
Stan Konnov <stanis.konnov@gmail.com>
2024-09-09 20:27:44 +0200

    Continue with unit tests

--- tests/apollo/calculations/test_vix_futures_convergence_divergence.py
+++ tests/apollo/calculations/test_vix_futures_convergence_divergence.py
@@ -83,3 +83,66 @@ def test__calculate_vix_futures_convergence_divergence__for_correct_vixep_calcul
         control_dataframe["vix_spf_pct_diff"],
         enhanced_dataframe["vix_spf_pct_diff"],
     )
+
+
+@pytest.mark.usefixtures("enhanced_dataframe", "window_size")
+def test__calculate_vix_futures_convergence_divergence__for_missing_data_calculation(
+    enhanced_dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_vix_futures_convergence_divergence for missing data calculation.
+
+    Resulting "vix_spf_pct_diff" column must have 0.0 for rows with missing data.
+    Resulting "vix_spf_pct_diff" column must have correct values for rows with data.
+    """
+
+    enhanced_dataframe.reset_index(inplace=True)
+
+    # Mimic missing data for "vix close" and "spf close" columns
+    enhanced_dataframe.loc[0:5, "vix close"] = MISSING_DATA_PLACEHOLDER
+    enhanced_dataframe.loc[0:5, "spf close"] = MISSING_DATA_PLACEHOLDER
+
+    control_dataframe = enhanced_dataframe.copy()
+
+    control_dataframe["vix_pct_change"] = 0.0
+    control_dataframe["spf_pct_change"] = 0.0
+    control_dataframe["vix_spf_pct_diff"] = 0.0
+
+    control_dataframe.loc[
+        control_dataframe["vix close"] != MISSING_DATA_PLACEHOLDER,
+        "vix_pct_change",
+    ] = control_dataframe["vix close"].pct_change()
+
+    control_dataframe.loc[
+        control_dataframe["spf close"] != MISSING_DATA_PLACEHOLDER,
+        "spf_pct_change",
+    ] = control_dataframe["spf close"].pct_change()
+
+    control_dataframe["vix_spf_pct_diff"] = (
+        control_dataframe["vix_pct_change"] - control_dataframe["spf_pct_change"]
+    )
+
+    control_dataframe["prev_vix_spf_pct_diff"] = control_dataframe[
+        "vix_spf_pct_diff"
+    ].shift(1)
+
+    control_dataframe.drop(
+        columns=["vix_pct_change", "spf_pct_change"],
+        inplace=True,
+    )
+
+    vfcd_calculator = VixFuturesConvergenceDivergenceCalculator(
+        dataframe=enhanced_dataframe,
+        window_size=window_size,
+    )
+
+    vfcd_calculator.calculate_vix_futures_convergence_divergence()
+
+    assert all(enhanced_dataframe["vix_spf_pct_diff"].iloc[0:5] == 0.0)
+
+    assert (
+        control_dataframe["vix_spf_pct_diff"]
+        .iloc[0:5]
+        .equals(enhanced_dataframe["vix_spf_pct_diff"].iloc[0:5])
+    )
