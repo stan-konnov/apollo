d7db209cc62a36ba05ad86538b9b0d4a6b88b324
Stan Konnov <stanis.konnov@gmail.com>
2024-09-09 19:17:48 +0200

    Dishing out unit tests for new calculators

--- tests/apollo/calculations/test_engulfing_vix_pattern.py
+++ tests/apollo/calculations/test_engulfing_vix_pattern.py
@@ -26,40 +26,16 @@ def test__calculate_engulfing_vix_pattern__for_correct_columns(
     Resulting dataframe must drop "vix_prev_open" and "vix_prev_close" columns.
     """
 
-    cvec_calculator = EngulfingVIXPatternCalculator(
+    evp_calculator = EngulfingVIXPatternCalculator(
         dataframe=enhanced_dataframe,
         window_size=window_size,
     )
 
-    cvec_calculator.calculate_engulfing_vix_pattern()
+    evp_calculator.calculate_engulfing_vix_pattern()
 
     assert "vixep" in enhanced_dataframe.columns
     assert "vix_prev_open" not in enhanced_dataframe.columns
     assert "vix_prev_close" not in enhanced_dataframe.columns
-
-
-@pytest.mark.usefixtures("enhanced_dataframe", "window_size")
-def test__calculate_vix_expansion_contraction__for_correct_rolling_window(
-    enhanced_dataframe: pd.DataFrame,
-    window_size: int,
-) -> None:
-    """
-    Test calculate_vix_expansion_contraction method for correct rolling window.
-
-    Where N = WINDOW_SIZE.
-
-    Resulting dataframe must skip WINDOW_SIZE - 1 rows for "cvec" column
-    Since Contraction Expansion calculation must have at least N rows to be calculated.
-    """
-
-    cvec_calculator = EngulfingVIXPatternCalculator(
-        dataframe=enhanced_dataframe,
-        window_size=window_size,
-    )
-
-    cvec_calculator.calculate_vix_expansion_contraction()
-
-    assert enhanced_dataframe["cvec"].isna().sum() == window_size - 1
 
 
 @pytest.mark.usefixtures("enhanced_dataframe", "window_size")
