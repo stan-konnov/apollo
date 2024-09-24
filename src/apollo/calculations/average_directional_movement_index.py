555a5bacdcd65847ac486a176abed72f81c84859
Stan Konnov <stanis.konnov@gmail.com>
2024-09-24 17:55:00 +0200

    Literature reference

--- src/apollo/calculations/average_directional_movement_index.py
+++ src/apollo/calculations/average_directional_movement_index.py
@@ -4,7 +4,12 @@ from apollo.calculations.base_calculator import BaseCalculator
 
 
 class AverageDirectionalMovementIndexCalculator(BaseCalculator):
-    """Average Directional Movement Index Calculator."""
+    """
+    Average Directional Movement Index Calculator.
+
+    Kaufman, Trading Systems and Methods, 2020, 6th ed.
+    Wilder, "Selection and Direction", Technical Analysis in Commodities, 1980.
+    """
 
     def __init__(self, dataframe: pd.DataFrame, window_size: int) -> None:
         """
