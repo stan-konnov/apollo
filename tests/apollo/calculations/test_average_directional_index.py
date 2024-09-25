e2dbfb0f445e45320f71dd28a00f5ef8096a76d6
Stan Konnov <stanis.konnov@gmail.com>
2024-09-25 20:56:18 +0200

    Dishing out unit tests

--- tests/apollo/calculations/test_average_directional_index.py
+++ tests/apollo/calculations/test_average_directional_index.py
@@ -86,3 +86,117 @@ def test__calculate_average_directional_movement_index__for_correct_rolling_wind
     assert dataframe["pdi"].isna().sum() == (window_size - 1) * 2
     assert dataframe["mdi"].isna().sum() == (window_size - 1) * 2
     assert dataframe["dx_adx_ampl"].isna().sum() == (window_size - 1) * 3
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_average_directional_movement_index__for_correct_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_average_directional_movement_index method for correct calculation.
+
+    Following columns must have correct values for each row:
+    "dx", "pdi", "mdi", "prev_pdi", "prev_mdi", "dx_adx_ampl", "prev_dx_adx_ampl".
+    """
+
+    dataframe = precalculate_shared_values(dataframe)
+
+    control_dataframe = dataframe.copy()
+
+    atr_calculator = AverageTrueRangeCalculator(
+        dataframe=control_dataframe,
+        window_size=window_size,
+    )
+    atr_calculator.calculate_average_true_range()
+
+    control_dataframe["prev_low"] = control_dataframe["adj low"].shift(1)
+    control_dataframe["prev_high"] = control_dataframe["adj high"].shift(1)
+
+    control_dataframe["mdm"] = (
+        control_dataframe["adj low"] - control_dataframe["prev_low"]
+    )
+    control_dataframe["pdm"] = (
+        control_dataframe["adj high"] - control_dataframe["prev_high"]
+    )
+
+    control_dataframe["mdm"] = (
+        control_dataframe["mdm"]
+        .ewm(
+            alpha=1 / window_size,
+            min_periods=window_size,
+            adjust=False,
+        )
+        .mean()
+    )
+    control_dataframe["pdm"] = (
+        control_dataframe["pdm"]
+        .ewm(
+            alpha=1 / window_size,
+            min_periods=window_size,
+            adjust=False,
+        )
+        .mean()
+    )
+
+    control_dataframe["pdi"] = control_dataframe["pdm"] / control_dataframe["atr"]
+    control_dataframe["mdi"] = control_dataframe["mdm"] / control_dataframe["atr"]
+
+    control_dataframe["dx"] = 100 * (
+        abs(control_dataframe["pdi"] - control_dataframe["mdi"])
+        / (control_dataframe["pdi"] + control_dataframe["mdi"])
+    )
+
+    control_dataframe["adx"] = (
+        control_dataframe["dx"]
+        .ewm(
+            alpha=1 / window_size,
+            min_periods=window_size,
+            adjust=False,
+        )
+        .mean()
+    )
+
+    control_dataframe["dx_adx_ampl"] = abs(control_dataframe["dx"]) - abs(
+        control_dataframe["adx"],
+    )
+
+    control_dataframe["prev_pdi"] = control_dataframe["pdi"].shift(1)
+    control_dataframe["prev_mdi"] = control_dataframe["mdi"].shift(1)
+
+    control_dataframe["prev_dx"] = control_dataframe["dx"].shift(1)
+    control_dataframe["prev_dx_adx_ampl"] = control_dataframe["dx_adx_ampl"].shift(1)
+
+    control_dataframe.drop(
+        columns=["prev_low", "prev_high", "mdm", "pdm", "adx"],
+        inplace=True,
+    )
+
+    atr_calculator = AverageTrueRangeCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+    atr_calculator.calculate_average_true_range()
+
+    adx_calculator = AverageDirectionalMovementIndexCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+    adx_calculator.calculate_average_directional_movement_index()
+
+    pd.testing.assert_series_equal(dataframe["dx"], control_dataframe["dx"])
+
+    pd.testing.assert_series_equal(dataframe["pdi"], control_dataframe["pdi"])
+    pd.testing.assert_series_equal(dataframe["mdi"], control_dataframe["mdi"])
+
+    pd.testing.assert_series_equal(dataframe["prev_pdi"], control_dataframe["prev_pdi"])
+    pd.testing.assert_series_equal(dataframe["prev_mdi"], control_dataframe["prev_mdi"])
+
+    pd.testing.assert_series_equal(
+        dataframe["dx_adx_ampl"],
+        control_dataframe["dx_adx_ampl"],
+    )
+    pd.testing.assert_series_equal(
+        dataframe["prev_dx_adx_ampl"],
+        control_dataframe["prev_dx_adx_ampl"],
+    )
