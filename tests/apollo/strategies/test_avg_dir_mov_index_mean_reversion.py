50e857cfc19993cd24684a5cc4856926fcdc46d5
Stan Konnov <stanis.konnov@gmail.com>
2024-09-25 21:12:14 +0200

    Dishing out unit tests

--- tests/apollo/strategies/test_avg_dir_mov_index_mean_reversion.py
+++ tests/apollo/strategies/test_avg_dir_mov_index_mean_reversion.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import pytest
 
-from apollo.calculations.average_true_range import AverageTrueRangeCalculator
-from apollo.calculations.combinatory_futures_patterns import (
-    CombinatoryFuturesPatternsCalculator,
+from apollo.calculations.average_directional_movement_index import (
+    AverageDirectionalMovementIndexCalculator,
 )
+from apollo.calculations.average_true_range import AverageTrueRangeCalculator
 from apollo.calculations.engulfing_vix_pattern import EngulfingVIXPatternCalculator
 from apollo.settings import LONG_SIGNAL, NO_SIGNAL, SHORT_SIGNAL
-from apollo.strategies.combinatory_futures_patterns import (
-    CombinatoryFuturesPatterns,
+from apollo.strategies.avg_dir_mov_index_mean_reversion import (
+    AverageDirectionalMovementIndexMeanReversion,
 )
 from tests.utils.precalculate_shared_values import precalculate_shared_values
 
 
 @pytest.mark.usefixtures("enhanced_dataframe", "window_size")
-def test__combinatory_futures_patterns__with_valid_parameters(
+def test__average_directional_movement_index_mean_reversion__with_valid_parameters(
     enhanced_dataframe: pd.DataFrame,
     window_size: int,
 ) -> None:
     """
-    Test Combinatory Futures Patterns Strategy with valid parameters.
+    Test Average Directional Movement Index Mean Reversion with valid parameters.
 
     Strategy should properly calculate trading signals.
     """
@@ -42,12 +42,11 @@ def test__combinatory_futures_patterns__with_valid_parameters(
     )
     evp_calculator.calculate_engulfing_vix_pattern()
 
-    cfp_calculator = CombinatoryFuturesPatternsCalculator(
+    adx_calculator = AverageDirectionalMovementIndexCalculator(
         dataframe=control_dataframe,
         window_size=window_size,
-        doji_threshold=0.005,
     )
-    cfp_calculator.calculate_combinatory_futures_patterns()
+    adx_calculator.calculate_average_directional_movement_index()
 
     control_dataframe.loc[
         control_dataframe["vix_ep"] == evp_calculator.BULLISH_ENGULFING,
@@ -61,15 +60,13 @@ def test__combinatory_futures_patterns__with_valid_parameters(
 
     control_dataframe.loc[
         (
-            (control_dataframe["spf_ep"] == cfp_calculator.BEARISH_PATTERN)
+            (control_dataframe["adj close"] < control_dataframe["prev_close"])
+            & (control_dataframe["dx"] < control_dataframe["prev_dx"])
+            & (control_dataframe["dx_adx_ampl"] < control_dataframe["prev_dx_adx_ampl"])
             | (
                 (control_dataframe["adj close"] < control_dataframe["prev_close"])
-                & (control_dataframe["spf_ep_tm1"] == cfp_calculator.BULLISH_PATTERN)
-            )
-            | (control_dataframe["spf_tp"] == cfp_calculator.BEARISH_PATTERN)
-            | (
-                (control_dataframe["adj close"] < control_dataframe["prev_close"])
-                & (control_dataframe["spf_sp_tm1"] == cfp_calculator.BULLISH_PATTERN)
+                & (control_dataframe["pdi"] > control_dataframe["prev_pdi"])
+                & (control_dataframe["mdi"] > control_dataframe["prev_mdi"])
             )
         )
         | (control_dataframe["vix_signal"] == LONG_SIGNAL),
@@ -78,15 +75,13 @@ def test__combinatory_futures_patterns__with_valid_parameters(
 
     control_dataframe.loc[
         (
-            (control_dataframe["spf_ep"] == cfp_calculator.BULLISH_PATTERN)
+            (control_dataframe["adj close"] > control_dataframe["prev_close"])
+            & (control_dataframe["dx"] > control_dataframe["prev_dx"])
+            & (control_dataframe["dx_adx_ampl"] > control_dataframe["prev_dx_adx_ampl"])
             | (
                 (control_dataframe["adj close"] > control_dataframe["prev_close"])
-                & (control_dataframe["spf_ep_tm1"] == cfp_calculator.BEARISH_PATTERN)
-            )
-            | (control_dataframe["spf_tp"] == cfp_calculator.BULLISH_PATTERN)
-            | (
-                (control_dataframe["adj close"] > control_dataframe["prev_close"])
-                & (control_dataframe["spf_sp_tm1"] == cfp_calculator.BEARISH_PATTERN)
+                & (control_dataframe["pdi"] < control_dataframe["prev_pdi"])
+                & (control_dataframe["mdi"] < control_dataframe["prev_mdi"])
             )
         )
         | (control_dataframe["vix_signal"] == SHORT_SIGNAL),
@@ -95,13 +90,12 @@ def test__combinatory_futures_patterns__with_valid_parameters(
 
     control_dataframe.dropna(inplace=True)
 
-    combinatory_futures_patterns = CombinatoryFuturesPatterns(
+    avg_dir_mov_index_mean_reversion = AverageDirectionalMovementIndexMeanReversion(
         enhanced_dataframe,
         window_size,
-        doji_threshold=0.005,
     )
 
-    combinatory_futures_patterns.model_trading_signals()
+    avg_dir_mov_index_mean_reversion.model_trading_signals()
 
     pd.testing.assert_series_equal(
         control_dataframe["signal"],
