39a17c88cd416811358ad1e8845238d5cf7e713d
Stan Konnov <stanis.konnov@gmail.com>
2024-09-25 20:31:12 +0200

    Dishing out unit tests

--- tests/apollo/calculations/test_average_directional_index.py
+++ tests/apollo/calculations/test_average_directional_index.py
@@ -50,3 +50,39 @@ def test__calculate_average_directional_movement_index__for_correct_columns(
     assert "adx" not in dataframe.columns
     assert "prev_low" not in dataframe.columns
     assert "prev_high" not in dataframe.columns
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_average_directional_movement_index__for_correct_rolling_window(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_average_directional_movement_index method for correct rolling window.
+
+    Where N = WINDOW_SIZE.
+
+    Resulting dataframe must skip (WINDOW_SIZE - 1) * 3 rows
+    for dx_adx_ampl and (WINDOW_SIZE - 1) * 2 rows for pdi, mdi.
+    Since Average Directional Movement Index Amplitude is calculated
+    based on ADX, and, therefore PDI and MDI that are calculated based on ATR,
+    which calculation must have at least N rows of valid True Range calculation.
+    """
+
+    dataframe = precalculate_shared_values(dataframe)
+
+    atr_calculator = AverageTrueRangeCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+    atr_calculator.calculate_average_true_range()
+
+    adx_calculator = AverageDirectionalMovementIndexCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+    adx_calculator.calculate_average_directional_movement_index()
+
+    assert dataframe["pdi"].isna().sum() == (window_size - 1) * 2
+    assert dataframe["mdi"].isna().sum() == (window_size - 1) * 2
+    assert dataframe["dx_adx_ampl"].isna().sum() == (window_size - 1) * 3
