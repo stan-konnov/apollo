219c70e1c2e45641e52dc4b21fe971d0d8a10b69
Stan Konnov <stanis.konnov@gmail.com>
2024-10-14 20:43:06 +0200

    Unit tests for calculator

--- tests/apollo/calculations/test_elliot_waves.py
+++ tests/apollo/calculations/test_elliot_waves.py
@@ -1,12 +1,22 @@
+import numpy as np
 import pandas as pd
 import pytest
 
-from apollo.calculations.average_true_range import AverageTrueRangeCalculator
 from apollo.calculations.elliot_waves import ElliotWavesCalculator
 from tests.utils.precalculate_shared_values import precalculate_shared_values
 
-FAST_OSCILLATOR_PERIOD = 5
-SLOW_OSCILLATOR_PERIOD = 10
+GOLDEN_RATIO: float = 1.618
+
+NO_VALUE: float = 0.0
+
+UP_TREND: float = 1.0
+DOWN_TREND: float = -1.0
+
+UPWARD_WAVE: float = 1.0
+DOWNWARD_WAVE: float = -1.0
+
+FAST_OSCILLATOR_PERIOD: int = 5
+SLOW_OSCILLATOR_PERIOD: int = 10
 
 
 @pytest.mark.usefixtures("dataframe", "window_size")
@@ -24,12 +34,6 @@ def test__calculate_elliot_waves__for_correct_columns(
     """
 
     dataframe = precalculate_shared_values(dataframe)
-
-    atr_calculator = AverageTrueRangeCalculator(
-        dataframe=dataframe,
-        window_size=window_size,
-    )
-    atr_calculator.calculate_average_true_range()
 
     ew_calculator = ElliotWavesCalculator(
         dataframe=dataframe,
@@ -64,12 +68,6 @@ def test__calculate_elliot_waves__for_correct_rolling_window(
 
     dataframe = precalculate_shared_values(dataframe)
 
-    atr_calculator = AverageTrueRangeCalculator(
-        dataframe=dataframe,
-        window_size=window_size,
-    )
-    atr_calculator.calculate_average_true_range()
-
     ew_calculator = ElliotWavesCalculator(
         dataframe=dataframe,
         window_size=window_size,
@@ -79,3 +77,146 @@ def test__calculate_elliot_waves__for_correct_rolling_window(
     ew_calculator.calculate_elliot_waves()
 
     assert dataframe["ew"].isna().sum() == window_size - 1
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_elliot_waves__for_correct_elliot_waves_calculation(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_elliot_waves method for correct Elliot Waves calculation.
+
+    Resulting "ew" column must have correct values for each row.
+    """
+
+    dataframe = precalculate_shared_values(dataframe)
+
+    elliot_waves = np.full((1, window_size - 1), np.nan).flatten().tolist()
+    elliot_waves_trend = np.full((1, window_size - 1), np.nan).flatten().tolist()
+
+    control_dataframe = dataframe.copy()
+    control_dataframe.reset_index(inplace=True)
+
+    control_dataframe["high_low_avg"] = (
+        control_dataframe["adj high"] + control_dataframe["adj low"]
+    ) / 2
+
+    control_dataframe["fast_hla_sma"] = (
+        control_dataframe["high_low_avg"]
+        .rolling(
+            window=FAST_OSCILLATOR_PERIOD,
+            min_periods=FAST_OSCILLATOR_PERIOD,
+        )
+        .mean()
+    )
+
+    control_dataframe["slow_hla_sma"] = (
+        control_dataframe["high_low_avg"]
+        .rolling(
+            window=SLOW_OSCILLATOR_PERIOD,
+            min_periods=SLOW_OSCILLATOR_PERIOD,
+        )
+        .mean()
+    )
+
+    control_dataframe["ewo"] = (
+        control_dataframe["fast_hla_sma"] - control_dataframe["slow_hla_sma"]
+    )
+
+    control_dataframe["ewo_sma"] = (
+        control_dataframe["ewo"]
+        .rolling(
+            window=window_size,
+            min_periods=window_size,
+        )
+        .mean()
+    )
+
+    control_dataframe["adj close"].rolling(window_size).apply(
+        mimic_calc_elliot_waves,
+        args=(
+            control_dataframe,
+            elliot_waves,
+            elliot_waves_trend,
+        ),
+    )
+
+    control_dataframe["ew"] = elliot_waves
+    control_dataframe.set_index("date", inplace=True)
+
+    ew_calculator = ElliotWavesCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+        fast_oscillator_period=FAST_OSCILLATOR_PERIOD,
+        slow_oscillator_period=SLOW_OSCILLATOR_PERIOD,
+    )
+    ew_calculator.calculate_elliot_waves()
+
+    pd.testing.assert_series_equal(dataframe["ew"], control_dataframe["ew"])
+
+
+def mimic_calc_elliot_waves(
+    series: pd.Series,
+    dataframe: pd.DataFrame,
+    elliot_waves: list[float],
+    elliot_waves_trend: list[float],
+) -> float:
+    """
+    Mimicry of Elliot Waves calculation for testing purposes.
+
+    Please see ElliotWavesCalculator for
+    detailed explanation of Elliot Waves calculation.
+    """
+
+    rolling_df = dataframe.loc[series.index]
+
+    curr_wave = None
+    curr_trend = None
+
+    ewo_h = rolling_df["ewo"].max()
+    ewo_l = rolling_df["ewo"].min()
+
+    curr_ewo = rolling_df.iloc[-1]["ewo"]
+    curr_ewo_sma = rolling_df.iloc[-1]["ewo_sma"]
+
+    prev_trend = elliot_waves_trend[rolling_df.index[-2]]
+
+    no_prev_trend = prev_trend == NO_VALUE or np.isnan(prev_trend)
+
+    if no_prev_trend and curr_ewo == ewo_h:
+        curr_trend = UP_TREND
+
+    if (
+        curr_ewo < curr_ewo_sma
+        and prev_trend == DOWN_TREND
+        and curr_ewo > GOLDEN_RATIO * ewo_l
+    ):
+        curr_trend = UP_TREND
+
+    if no_prev_trend and curr_ewo == ewo_l:
+        curr_trend = DOWN_TREND
+
+    if (
+        curr_ewo > curr_ewo_sma
+        and prev_trend == UP_TREND
+        and curr_ewo < GOLDEN_RATIO * ewo_h
+    ):
+        curr_trend = DOWN_TREND
+
+    if curr_trend == UP_TREND and curr_ewo == ewo_h:
+        curr_wave = UPWARD_WAVE
+
+    if curr_trend == UP_TREND and curr_ewo == ewo_l:
+        curr_wave = DOWNWARD_WAVE
+
+    if curr_trend == DOWN_TREND and curr_ewo == ewo_l:
+        curr_wave = UPWARD_WAVE
+
+    if curr_trend == DOWN_TREND and curr_ewo == ewo_h:
+        curr_wave = DOWNWARD_WAVE
+
+    elliot_waves.append(curr_wave or NO_VALUE)
+    elliot_waves_trend.append(curr_trend or NO_VALUE)
+
+    return 0.0
