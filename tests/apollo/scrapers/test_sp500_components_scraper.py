c84e9968c82985ff3c9f4aa6508ca7bebdedc188
Stan Konnov <stanis.konnov@gmail.com>
2024-10-22 19:27:47 +0200

    And more unit tests for the scraper

--- tests/apollo/scrapers/test_sp500_components_scraper.py
+++ tests/apollo/scrapers/test_sp500_components_scraper.py
@@ -234,3 +234,37 @@ def test__sp500_components_scraper__for_raising_if_tickers_are_malformed(
         sp500_components_scraper.scrape_sp500_components()
 
     assert str(exception.value) == exception_message
+
+
+@pytest.mark.parametrize(
+    "requests_get_call",
+    ["apollo.scrapers.sp500_components_scraper.get"],
+    indirect=True,
+)
+@pytest.mark.usefixtures("sp500_components_page")
+def test__sp500_components_scraper__for_scraping_list_of_sp500_tickers(
+    requests_get_call: Mock,
+    sp500_components_page: str,
+) -> None:
+    """
+    Test SP500 Components Scraper for scraping list of S&P 500 tickers.
+
+    Scraper must return a list of strings representing S&P 500 components.
+
+    Returned list length must be equal to the number of S&P 500 components.
+    Returned list element must not include any non-alphanumeric characters.
+    """
+
+    # 2024-10-22:
+    # Index contains 503 stocks
+    # because it includes two share classes
+    # of stock from 4 of its component companies
+    current_number_of_tickers = 503
+
+    requests_get_call.return_value = Mock(text=sp500_components_page)
+    sp500_components_scraper = SP500ComponentsScraper()
+
+    sp500_components_tickers = sp500_components_scraper.scrape_sp500_components()
+
+    assert len(sp500_components_tickers) == current_number_of_tickers
+    assert all(ticker.isalnum() for ticker in sp500_components_tickers)
