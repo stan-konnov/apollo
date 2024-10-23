4ba091b82c8d24befa0790d6135b84721d6b9643
Stan Konnov <stanis.konnov@gmail.com>
2024-10-23 20:15:40 +0200

    A setup for screening

--- src/apollo/screening/ticker_screener.py
+++ src/apollo/screening/ticker_screener.py
@@ -1,3 +1,6 @@
+from apollo.scrapers.sp500_components_scraper import SP500ComponentsScraper
+
+
 class TickerScreener:
     """
     Ticker Screener class.
@@ -6,7 +9,16 @@ class TickerScreener:
     based on the measures of volatility and noise with the
     purpose of identifying the most suitable instrument to trade.
 
-    Makes use of SP500ComponentsScraper to get the list of S&P500 tickers for screening.
+    Makes use of S&P 500 Components Scraper to get the list of S&P500 tickers.
 
     Is multiprocessing capable and runs in parallel.
     """
+
+    def __init__(self) -> None:
+        """
+        Construct Ticker Screener.
+
+        Initializes S&P500 Components Scraper.
+        """
+
+        self._sp500_components_scraper = SP500ComponentsScraper()
