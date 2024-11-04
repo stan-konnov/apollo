e068a565e3de0f1bd020e23a121e2fed31c60f44
Stan Konnov <stanis.konnov@gmail.com>
2024-11-04 19:54:34 +0100

    Work in progress on screener tests

--- tests/apollo/screening/test_ticker_screener.py
+++ tests/apollo/screening/test_ticker_screener.py
@@ -1,9 +1,11 @@
+from unittest import mock
 from unittest.mock import Mock
 
 import pandas as pd
 import pytest
 
 from apollo.screening.ticker_screener import TickerScreener
+from apollo.settings import END_DATE, FREQUENCY, MAX_PERIOD, START_DATE
 
 
 @pytest.mark.usefixtures("dataframe", "window_size")
@@ -31,22 +33,34 @@ def test__calculate_measures__for_correctly_calculating_screening_measures(
     ticker_screener._sp500_components_scraper = Mock()  # noqa: SLF001
 
     # Mimic the return value of Price Data Provider
-    ticker_screener._price_data_provider.get_prices.return_value = dataframe  # noqa: SLF001
+    ticker_screener._price_data_provider.get_price_data.return_value = dataframe  # noqa: SLF001
 
     ticker_screener._calculate_measures(tickers)  # noqa: SLF001
 
-    ticker_screener._price_data_provider.get_prices.assert_has_calls(  # noqa: SLF001
+    ticker_screener._price_data_provider.get_price_data.assert_has_calls(  # noqa: SLF001
         [
-            tickers[0],
-            tickers[1],
+            mock.call(
+                ticker=tickers[0],
+                frequency=str(FREQUENCY),
+                start_date=str(START_DATE),
+                end_date=str(END_DATE),
+                max_period=bool(MAX_PERIOD),
+            ),
+            mock.call(
+                ticker=tickers[1],
+                frequency=str(FREQUENCY),
+                start_date=str(START_DATE),
+                end_date=str(END_DATE),
+                max_period=bool(MAX_PERIOD),
+            ),
         ],
         any_order=True,
     )
 
-    ticker_screener._api_connector.get_earnings.assert_has_calls(  # noqa: SLF001
+    ticker_screener._api_connector.request_upcoming_earnings_date.assert_has_calls(  # noqa: SLF001
         [
-            tickers[0],
-            tickers[1],
+            mock.call(tickers[0]),
+            mock.call(tickers[1]),
         ],
         any_order=True,
     )
