84dc66e03040187671888954cb4f6dfb3411f65a
Stan Konnov <stanis.konnov@gmail.com>
2024-11-04 19:19:23 +0100

    Dishing out tests for Kaufman Efficiency Ratio Calculator

--- tests/apollo/calculations/test_kaufman_efficiency_ratio.py
+++ tests/apollo/calculations/test_kaufman_efficiency_ratio.py
@@ -24,3 +24,26 @@ def test__calculate_kaufman_efficiency_ratio__for_correct_columns(
     ker_calculator.calculate_kaufman_efficiency_ratio()
 
     assert "ker" in dataframe.columns
+
+
+@pytest.mark.usefixtures("dataframe", "window_size")
+def test__calculate_kaufman_efficiency_ratio_for_correct_rolling_window(
+    dataframe: pd.DataFrame,
+    window_size: int,
+) -> None:
+    """
+    Test calculate_kaufman_efficiency_ratio method for correct rolling window.
+
+    Where N = WINDOW_SIZE.
+
+    Resulting dataframe must skip WINDOW_SIZE rows for ker column
+    Since KER calculation relies on prices from one window back.
+    """
+    ker_calculator = KaufmanEfficiencyRatioCalculator(
+        dataframe=dataframe,
+        window_size=window_size,
+    )
+
+    ker_calculator.calculate_kaufman_efficiency_ratio()
+
+    assert dataframe["ker"].isna().sum() == window_size
