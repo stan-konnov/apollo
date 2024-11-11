c108f9c5d1eda1d67e5f96a81d7b2ade0a174ec8
Stan Konnov <stanis.konnov@gmail.com>
2024-11-11 22:41:52 +0100

    Removing unnecessary migration = overthinking

--- prisma/migrations/20241111184520_add_id_ticker_status_unique_constraint_on_positions/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[id,ticker,status]` on the table `positions` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "positions_id_ticker_status_key" ON "positions"("id", "ticker", "status");
