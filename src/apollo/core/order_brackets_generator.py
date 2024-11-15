0a19d49011322bc3ea8957787e11eb0a150ac0d9
Stan Konnov <stanis.konnov@gmail.com>
2024-11-15 15:07:41 +0100

    Setup for order brackets calculation

---
+++ src/apollo/core/order_brackets_generator.py
@@ -0,0 +1,7 @@
+class OrderBracketsGenerator:
+    """
+    Order Brackets Generator class.
+
+    Exists to calculate Limit Entry Price, Stop Loss, and
+    Take Profit levels during backtesting and signal dispatching.
+    """
