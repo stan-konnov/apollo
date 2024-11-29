903c809444f488b93c1c5793991e63a1585a2c61
Stan Konnov <stanis.konnov@gmail.com>
2024-11-29 16:11:41 +0100

    Docstrings

--- src/apollo/processors/signal_dispatcher.py
+++ src/apollo/processors/signal_dispatcher.py
@@ -30,8 +30,10 @@ class SignalDispatcher:
         """
         Construct Signal Dispatcher.
 
+        Initialize Configuration.
         Initialize Database Connector.
         Initialize Price Data Provider.
+        Initialize Price Data Enhancer.
         """
 
         self._configuration = Configuration()
