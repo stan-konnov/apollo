c2ce2ecee5c297bd2604bf3304d6da852a7433ea
Stan Konnov <stanis.konnov@gmail.com>
2024-12-23 13:23:26 +0100

    Just a setup for signal generation runner

---
+++ src/apollo/core/signal_generator.py
@@ -0,0 +1,7 @@
+class SignalGenerator:
+    """
+    Signal Generator class.
+
+    A meta class that encapsulates the signal generation logic.
+    Orchestrates screening, optimization, and dispatching of signals.
+    """
