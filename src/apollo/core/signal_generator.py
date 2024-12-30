62b4537c510cd4050126970ff355297419f7809e
Stan Konnov <stanis.konnov@gmail.com>
2024-12-30 18:42:32 +0100

    Into cron job with while loop to make trigger more flexible

--- src/apollo/core/signal_generator.py
+++ src/apollo/core/signal_generator.py
@@ -38,9 +38,7 @@ class SignalGenerator:
             ParameterOptimizerMode.MULTIPLE_STRATEGIES,
         )
 
-        # Flag to control the process
-        # amongst surrounding time checks
-        self._ran_today = False
+        self._running = True
 
     def generate_signals(self) -> None:
         """
@@ -49,66 +47,65 @@ class SignalGenerator:
         Run the signal generation process.
         """
 
-        # Get current point in time
-        # in the configured exchange
-        current_datetime_in_exchange = datetime.now(
-            tz=ZoneInfo(
-                EXCHANGE_TIME_ZONE_AND_HOURS[str(EXCHANGE)]["timezone"],
-            ),
-        )
+        while self._running:
+            # Get current point in time
+            # in the configured exchange
+            current_datetime_in_exchange = datetime.now(
+                tz=ZoneInfo(
+                    EXCHANGE_TIME_ZONE_AND_HOURS[str(EXCHANGE)]["timezone"],
+                ),
+            )
 
-        # Get close point in time
-        # in the configured exchange
-        close_time_in_exchange = datetime.strptime(
-            EXCHANGE_TIME_ZONE_AND_HOURS[str(EXCHANGE)]["hours"]["close"],
-            DEFAULT_TIME_FORMAT,
-        ).time()
+            # Get close point in time
+            # in the configured exchange
+            close_time_in_exchange = datetime.strptime(
+                EXCHANGE_TIME_ZONE_AND_HOURS[str(EXCHANGE)]["hours"]["close"],
+                DEFAULT_TIME_FORMAT,
+            ).time()
 
-        # Get open point in time
-        # in the configured exchange
-        open_time_in_exchange = datetime.strptime(
-            EXCHANGE_TIME_ZONE_AND_HOURS[str(EXCHANGE)]["hours"]["open"],
-            DEFAULT_TIME_FORMAT,
-        ).time()
+            # Get open point in time
+            # in the configured exchange
+            open_time_in_exchange = datetime.strptime(
+                EXCHANGE_TIME_ZONE_AND_HOURS[str(EXCHANGE)]["hours"]["open"],
+                DEFAULT_TIME_FORMAT,
+            ).time()
 
-        # Get NYSE market holidays calendar
-        market_holidays = mcal.get_calendar("NYSE").holidays().holidays  # type: ignore  # noqa: PGH003
+            # Get NYSE market holidays calendar
+            market_holidays = mcal.get_calendar("NYSE").holidays().holidays  # type: ignore  # noqa: PGH003
 
-        # Transform to regular python datetime objects
-        market_holidays = [to_datetime(str(holiday)) for holiday in market_holidays]
+            # Transform to regular python datetime objects
+            market_holidays = [to_datetime(str(holiday)) for holiday in market_holidays]
 
-        # And limit to dates of the current year
-        market_holidays = [
-            holiday.date()
-            for holiday in market_holidays
-            if holiday.year == current_datetime_in_exchange.year
-        ]
+            # And limit to dates of the current year
+            market_holidays = [
+                holiday.date()
+                for holiday in market_holidays
+                if holiday.year == current_datetime_in_exchange.year
+            ]
 
-        # If the process can run,
-        # and today is not a market holiday,
-        # and current point in time is after the
-        # close and before the market open, kick off the process
-        if (
-            not self._ran_today
-            and current_datetime_in_exchange.date() not in market_holidays
-            and current_datetime_in_exchange.time() >= close_time_in_exchange
-            and current_datetime_in_exchange.time() < open_time_in_exchange
-        ):
-            # Screen tickers
-            self._ticker_screener.process_in_parallel()
+            # If today is not a market holiday,
+            # and current point in time is after the
+            # close and before the market open, kick off the process
+            if (
+                current_datetime_in_exchange.date() not in market_holidays
+                and current_datetime_in_exchange.time() >= close_time_in_exchange
+                and current_datetime_in_exchange.time() < open_time_in_exchange
+            ):
+                # Screen tickers
+                self._ticker_screener.process_in_parallel()
 
-            # Optimize parameters for each strategy
-            self._parameter_optimizer.process_in_parallel()
+                # Optimize parameters for each strategy
+                self._parameter_optimizer.process_in_parallel()
 
-            # Dispatch signals
-            self._signal_dispatcher.dispatch_signals()
+                # Dispatch signals
+                self._signal_dispatcher.dispatch_signals()
 
-            # Flip controls
-            self._ran_today = True
+                # Flip controls
+                self._running = False
 
-        # Flip back after market open
-        if (
-            self._ran_today
-            and current_datetime_in_exchange.time() >= open_time_in_exchange
-        ):
-            self._ran_today = False
+            # Flip back after market open
+            if (
+                self._ran_today
+                and current_datetime_in_exchange.time() >= open_time_in_exchange
+            ):
+                self._ran_today = True
