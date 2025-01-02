1b93154683eb19e2951bb240a392d9b9a1610d4d
Stan Konnov <stanis.konnov@gmail.com>
2025-01-02 19:10:11 +0100

    More tests never hurts

--- tests/apollo/core/test_signal_generation_runner.py
+++ tests/apollo/core/test_signal_generation_runner.py
@@ -55,3 +55,30 @@ def test__run_signal_generation__for_correctly_skipping_the_process() -> None:
     signal_generation_runner._ticker_screener.process_in_parallel.assert_not_called()  # noqa: SLF001
     signal_generation_runner._parameter_optimizer.process_in_parallel.assert_not_called()  # noqa: SLF001
     signal_generation_runner._signal_dispatcher.dispatch_signals.assert_not_called()  # noqa: SLF001
+
+
+# Assume today date is Saturday, 2025-01-04
+# Assume current time is after 16:00 ET >= 20:00 UTC
+@timeout_decorator.timeout(3)
+@freeze_time("2025-01-04 21:00:00")
+def test__run_signal_generation__for_correctly_skipping_the_process_on_weekend() -> (
+    None
+):
+    """
+    Test Signal Generation Runner for correctly skipping the process.
+
+    Signal Generation Runner must correctly skip kick off the process.
+    """
+
+    signal_generation_runner = SignalGenerationRunner()
+
+    signal_generation_runner._ticker_screener = Mock()  # noqa: SLF001
+    signal_generation_runner._signal_dispatcher = Mock()  # noqa: SLF001
+    signal_generation_runner._parameter_optimizer = Mock()  # noqa: SLF001
+
+    with contextlib.suppress(timeout_decorator.TimeoutError):
+        signal_generation_runner.run_signal_generation()
+
+    signal_generation_runner._ticker_screener.process_in_parallel.assert_not_called()  # noqa: SLF001
+    signal_generation_runner._parameter_optimizer.process_in_parallel.assert_not_called()  # noqa: SLF001
+    signal_generation_runner._signal_dispatcher.dispatch_signals.assert_not_called()  # noqa: SLF001
