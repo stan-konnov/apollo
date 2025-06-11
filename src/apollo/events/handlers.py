fb61d3bedd17c9293fbf626d39d09c1588e69567
Stan Konnov <stanis.konnov@gmail.com>
2025-06-11 17:42:07 +0200

    Glueing order manager.

--- src/apollo/events/handlers.py
+++ src/apollo/events/handlers.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 
 from apollo.events.emitter import emitter
+from apollo.managers.order_manager import OrderManager
 from apollo.models.dispatchable_signal import DispatchableSignal
 from apollo.settings import Events
 
@@ -12,3 +13,8 @@ def handle_signal_generated_event(signal: DispatchableSignal) -> None:
     """Handle open position recalculated event."""
 
     logger.info(signal)
+
+    order_manager = OrderManager()
+
+    if signal.dispatched_position:
+        order_manager.handle_dispatched_position()
