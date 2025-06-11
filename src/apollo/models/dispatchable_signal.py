e8c26659236d3467b88cc3dab02d73e676df3bba
Stan Konnov <stanis.konnov@gmail.com>
2025-06-11 21:22:20 +0200

    Cleaning stuff little by little.

--- src/apollo/models/dispatchable_signal.py
+++ src/apollo/models/dispatchable_signal.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
-import numpy as np
-from pydantic import BaseModel, field_serializer
+from pydantic import BaseModel
 
 
 class PositionSignal(BaseModel):
@@ -15,31 +14,6 @@ class PositionSignal(BaseModel):
     stop_loss: float
     take_profit: float
     target_entry_price: float
-
-    # Declare custom serializers
-    # from numpy types to native Python types
-    # as Pydantic does not support numpy types directly
-
-    @field_serializer("direction")
-    def serialize_direction(self, value: np.int64) -> int:
-        """Convert numpy int64 to int for serialization."""
-        return int(value)
-
-    @field_serializer("stop_loss")
-    def serialize_stop_loss(self, value: np.float64) -> float:
-        """Convert numpy float64 to float for serialization."""
-        return float(value)
-
-    @field_serializer("take_profit")
-    def serialize_take_profit(self, value: np.float64) -> float:
-        """Convert numpy float64 to float for serialization."""
-
-        return float(value)
-
-    @field_serializer("target_entry_price")
-    def serialize_target_entry_price(self, value: np.float64) -> float:
-        """Convert numpy float64 to float for serialization."""
-        return float(value)
 
 
 class DispatchableSignal(BaseModel):
