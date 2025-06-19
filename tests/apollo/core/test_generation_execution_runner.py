453b1bc9cc6eaa65ad41edb71a99713a5a5a6366
Stan Konnov <stanis.konnov@gmail.com>
2025-06-19 21:17:05 +0200

    Patching tests.

--- tests/apollo/core/test_generation_execution_runner.py
+++ tests/apollo/core/test_generation_execution_runner.py
@@ -94,9 +94,9 @@ def test__run_signal_generation_execution__for_correctly_skipping_the_process_on
     None
 ):
     """
-    Test Generation Execution Runner for correctly skipping the process.
+    Test Generation Execution Runner for correctly skipping the process on MH.
 
-    Generation Execution Runner must correctly skip the process.
+    Generation Execution Runner must correctly skip the process on MH.
     """
 
     generation_execution_runner = GenerationExecutionRunner()
@@ -111,3 +111,62 @@ def test__run_signal_generation_execution__for_correctly_skipping_the_process_on
     generation_execution_runner._ticker_screener.screen_tickers.assert_not_called()  # noqa: SLF001
     generation_execution_runner._parameter_optimizer.optimize_parameters.assert_not_called()  # noqa: SLF001
     generation_execution_runner._signal_generator.generate_signals.assert_not_called()  # noqa: SLF001
+
+
+# Assume today date is Saturday, 2025-01-04
+# Assume current time is after 16:00 ET >= 20:00 UTC
+@timeout_decorator.timeout(3)
+@freeze_time("2025-01-04 21:00:00")
+def test__run_signal_generation_execution__for_correctly_kicking_off_the_process_on_the_weekend_overridden() -> (  # noqa: E501
+    None
+):
+    """
+    Test Generation Execution Runner for correctly kicking off the process on weekend.
+
+    Generation Execution Runner must correctly
+    kick off the process on weekend when overridden.
+    """
+
+    generation_execution_runner = GenerationExecutionRunner()
+
+    generation_execution_runner._ticker_screener = Mock()  # noqa: SLF001
+    generation_execution_runner._signal_generator = Mock()  # noqa: SLF001
+    generation_execution_runner._parameter_optimizer = Mock()  # noqa: SLF001
+
+    with contextlib.suppress(timeout_decorator.TimeoutError):
+        generation_execution_runner.run_signal_generation_execution(
+            override_market_timing=True,
+        )
+
+    generation_execution_runner._ticker_screener.screen_tickers.assert_any_call()  # noqa: SLF001
+    generation_execution_runner._parameter_optimizer.optimize_parameters.assert_any_call()  # noqa: SLF001
+    generation_execution_runner._signal_generator.generate_signals.assert_any_call()  # noqa: SLF001
+
+
+# Assume today date is Wednesday, 2025-01-01
+# Assume current time is after 16:00 ET >= 20:00 UTC
+@timeout_decorator.timeout(3)
+@freeze_time("2025-01-01, 21:00:00")
+def test__run_signal_generation_execution__for_correctly_kicking_off_the_process_on_mh_overridden() -> (  # noqa: E501
+    None
+):
+    """
+    Test Generation Execution Runner for correctly kicking off the process on MH.
+
+    Generation Execution Runner must correctly skip the process.
+    """
+
+    generation_execution_runner = GenerationExecutionRunner()
+
+    generation_execution_runner._ticker_screener = Mock()  # noqa: SLF001
+    generation_execution_runner._signal_generator = Mock()  # noqa: SLF001
+    generation_execution_runner._parameter_optimizer = Mock()  # noqa: SLF001
+
+    with contextlib.suppress(timeout_decorator.TimeoutError):
+        generation_execution_runner.run_signal_generation_execution(
+            override_market_timing=True,
+        )
+
+    generation_execution_runner._ticker_screener.screen_tickers.assert_any_call()  # noqa: SLF001
+    generation_execution_runner._parameter_optimizer.optimize_parameters.assert_any_call()  # noqa: SLF001
+    generation_execution_runner._signal_generator.generate_signals.assert_any_call()  # noqa: SLF001
