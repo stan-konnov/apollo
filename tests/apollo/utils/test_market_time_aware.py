0769557d55b1715a6b61568196093e95ed7aef0f
Stan Konnov <stanis.konnov@gmail.com>
2025-06-20 16:27:23 +0200

    Dishing out tests for utils.

--- tests/apollo/utils/test_market_time_aware.py
+++ tests/apollo/utils/test_market_time_aware.py
@@ -4,9 +4,8 @@ from apollo.utils.market_time_aware import MarketTimeAware
 
 
 # Assume today date is Friday, 2025-06-20
-# Assume current time is within trading hours
-@freeze_time("2024-07-12 16:00:00")
-def test__get_market_time_metrics__for_correctly_calculating_metrics_on_trading_day_trading_hours() -> (  # noqa: E501
+@freeze_time("2025-06-20 16:00:00")
+def test__get_market_time_metrics__for_correctly_calculating_metrics_on_trading_day() -> (  # noqa: E501
     None
 ):
     """Test _get_market_time_metrics method for correct metrics."""
@@ -16,4 +15,34 @@ def test__get_market_time_metrics__for_correctly_calculating_metrics_on_trading_
     market_time_metrics = market_time_aware._get_market_time_metrics()  # noqa: SLF001
 
     assert market_time_metrics.is_business_day is True
+    assert market_time_metrics.is_market_holiday is False
+
+
+# Assume today date is Thursday, 2025-06-19
+@freeze_time("2025-06-19 16:00:00")
+def test__get_market_time_metrics__for_correctly_calculating_metrics_on_non_trading_day() -> (  # noqa: E501
+    None
+):
+    """Test _get_market_time_metrics method for correct metrics."""
+
+    market_time_aware = MarketTimeAware()
+
+    market_time_metrics = market_time_aware._get_market_time_metrics()  # noqa: SLF001
+
+    assert market_time_metrics.is_business_day is True
+    assert market_time_metrics.is_market_holiday is True
+
+
+# Assume today date is Saturday, 2025-06-21
+@freeze_time("2025-06-21 16:00:00")
+def test__get_market_time_metrics__for_correctly_calculating_metrics_non_weekend() -> (
+    None
+):
+    """Test _get_market_time_metrics method for correct metrics."""
+
+    market_time_aware = MarketTimeAware()
+
+    market_time_metrics = market_time_aware._get_market_time_metrics()  # noqa: SLF001
+
+    assert market_time_metrics.is_business_day is False
     assert market_time_metrics.is_market_holiday is False
