ff519d4400eee8965845d31b7c468a407691867f
Stan Konnov <stanis.konnov@gmail.com>
2025-06-21 16:52:06 +0200

    Dishing out unit tests for dispatched position order manager.

--- tests/apollo/processors/execution/test_disp_pos_order_manager.py
+++ tests/apollo/processors/execution/test_disp_pos_order_manager.py
@@ -2,7 +2,10 @@ from unittest.mock import Mock
 
 import pytest
 
-from apollo.errors.system_invariants import OpenPositionAlreadyExistsError
+from apollo.errors.system_invariants import (
+    DispatchedPositionDoesNotExistError,
+    OpenPositionAlreadyExistsError,
+)
 from apollo.models.position import Position, PositionStatus
 from apollo.processors.execution.disp_pos_order_manager import (
     DispatchedPositionOrderManager,
@@ -43,6 +46,42 @@ def test__handle_dispatched_position__for_raising_error_if_open_position_exists(
 
     with pytest.raises(
         OpenPositionAlreadyExistsError,
+        match=exception_message,
+    ) as exception:
+        disp_pos_order_manager.handle_dispatched_position()
+
+    assert str(exception.value) == exception_message
+
+
+@pytest.mark.parametrize(
+    "trading_client",
+    ["apollo.processors.execution.base_order_manager.TradingClient"],
+    indirect=True,
+)
+@pytest.mark.usefixtures("trading_client")
+def test__handle_dispatched_position__for_raising_error_if_dispatched_position_does_not_exist() -> (  # noqa: E501
+    None
+):
+    """
+    Test handle_dispatched_position method for raising error.
+
+    Method must raise DispatchedPositionDoesNotExistError.
+    """
+
+    disp_pos_order_manager = DispatchedPositionOrderManager()
+
+    disp_pos_order_manager._database_connector = Mock()  # noqa: SLF001
+    disp_pos_order_manager._database_connector.get_position_by_status.return_value = (  # noqa: SLF001
+        None
+    )
+
+    exception_message = (
+        "Dispatched position does not exist while handling dispatched signal. "
+        "System invariant violated, position was not dispatched."
+    )
+
+    with pytest.raises(
+        DispatchedPositionDoesNotExistError,
         match=exception_message,
     ) as exception:
         disp_pos_order_manager.handle_dispatched_position()
