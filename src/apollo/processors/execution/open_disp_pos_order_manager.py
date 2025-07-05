e3dc1b401148de6ebec157b1adc3cbfe5a6a1150
Stan Konnov <stanis.konnov@gmail.com>
2025-07-05 19:58:42 +0200

    feature: Factoring in system invariants: checks, checks, and again checks.

--- src/apollo/processors/execution/open_disp_pos_order_manager.py
+++ src/apollo/processors/execution/open_disp_pos_order_manager.py
@@ -1,4 +1,13 @@
+from logging import getLogger
+
+from apollo.errors.system_invariants import (
+    DispatchedPositionDoesNotExistError,
+    OpenPositionDoesNotExistError,
+)
+from apollo.models.position import PositionStatus
 from apollo.processors.execution.base_order_manager import BaseOrderManager
+
+logger = getLogger(__name__)
 
 
 class OpenDispatchedPositionOrderManager(BaseOrderManager):
@@ -21,3 +30,29 @@ class OpenDispatchedPositionOrderManager(BaseOrderManager):
         Communicate with Alpaca API to fetch the status
         of the orders and synchronize them with positions in the database.
         """
+
+        logger.info("Handling open-dispatched position signal.")
+
+        # Query existing open position
+        existing_open_position = self._database_connector.get_position_by_status(
+            PositionStatus.OPEN,
+        )
+
+        # Raise error if open position does not exist
+        if not existing_open_position:
+            raise OpenPositionDoesNotExistError(
+                "Open position does not exist while handling OD signal. "
+                "System invariant violated, position was not opened.",
+            )
+
+        # Query existing dispatched position
+        existing_dispatched_position = self._database_connector.get_position_by_status(
+            PositionStatus.DISPATCHED,
+        )
+
+        # Raise error if no dispatched position exists
+        if not existing_dispatched_position:
+            raise DispatchedPositionDoesNotExistError(
+                "Dispatched position does not exist while handling OD signal. "
+                "System invariant violated, position was not dispatched.",
+            )
